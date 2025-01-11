5874be2719a3c926ac9480420fd00e2f1edc7a58
goteusz-maszyk <mateusz.goszyk@gmail.com>
2025-01-11 23:46:37 +0100

    feat: navbar

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,4 +1,29 @@
+<script setup>
+const navbarItems = {
+  Github: 'https://github.com/Hack4Krak/Hack4KrakSite',
+}
+</script>
+
 <template>
+  <nav class="bg-white dark:bg-gray-800 quantico">
+    <div class="max-w-7xl mx-auto py-3 flex justify-between items-center">
+      <a
+        class="text-2xl font-bold text-gray-800 dark:text-white flex items-center"
+        href="#"
+      ><img
+        src="/logo.svg"
+        class="max-h-12"
+      > Hack4Krak</a>
+      <div class="md:flex space-x-4">
+        <a
+          v-for="(url, text) in navbarItems"
+          :key="text"
+          :href="url"
+          class="text-gray-800 dark:text-white hover:text-blue-500"
+        >{{ text }}</a>
+      </div>
+    </div>
+  </nav>
   <div>
     <NuxtLayout>
       <NuxtPage />
