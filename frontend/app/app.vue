4866955648a37d92617f9f7594dcaa3e03955798
goteusz-maszyk <mateusz.goszyk@gmail.com>
2025-01-13 21:59:05 +0100

    misc fixes

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,30 +1,7 @@
-<script setup>
-const navbarItems = {
-  Github: 'https://github.com/Hack4Krak/Hack4KrakSite',
-}
-</script>
-
 <template>
-  <nav class="bg-white dark:bg-gray-800 quantico">
-    <div class="max-w-7xl mx-auto py-3 flex justify-between items-center">
-      <a
-        class="text-2xl font-bold text-gray-800 dark:text-white flex items-center"
-        href="#"
-      ><img
-        src="/logo.svg"
-        class="max-h-12"
-      > Hack4Krak</a>
-      <div class="md:flex space-x-4">
-        <a
-          v-for="(url, text) in navbarItems"
-          :key="text"
-          :href="url"
-          class="text-gray-800 dark:text-white hover:text-blue-500"
-        >{{ text }}</a>
-      </div>
-    </div>
-  </nav>
-  <div>
+  <NuxtLoadingIndicator />
+  <MainNavbar />
+  <div class="min-h-[calc(100vh-var(--header-height))]">
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
---
+++ frontend/app/components/MainLogo.vue
@@ -0,0 +1,7 @@
+<template>
+  <NuxtImg
+    src="/img/logo.svg"
+    class="max-h-12 not-dark:invert"
+    alt="Hack4Krak logo"
+  />
+</template>
