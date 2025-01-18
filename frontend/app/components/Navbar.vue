fd899eef65698ac36aad9601622b345817569c71
norbiros <norbiros@protonmail.com>
2025-01-18 18:38:42 +0100

    feat: Reimplemented navbar

--- frontend/app/components/Navbar.vue
+++ frontend/app/components/Navbar.vue
@@ -1,55 +1,80 @@
 <script setup lang="ts">
-const navbarItems = {
-  Kontakt: '#',
-  Discord: 'https://discord.gg/ASPqckzEd8',
-  Ranking: '#',
-  Regulamin: '#',
+const isMobileMenuOpen = ref(false)
+function toggleMobileMenu() {
+  isMobileMenuOpen.value = !isMobileMenuOpen.value
 }
 
-const isExpanded = ref(false)
-const router = useRouter()
-
-watch(() => router.currentRoute.value, () => {
-  isExpanded.value = false
-})
+const items = [
+  [
+    {
+      label: 'Kontakt',
+      to: '/contact',
+    },
+    {
+      label: 'Ranking',
+      to: '/ranking',
+    },
+    {
+      label: 'Regulamin',
+      to: '/rules',
+    },
+    {
+      label: 'Discord',
+      to: 'https://discord.gg/ASPqckzEd8',
+      target: '_blank',
+    },
+  ],
+  [
+    {
+      slot: 'button',
+      to: '/login',
+    },
+  ],
+]
 </script>
 
 <template>
-  <nav class="bg-black font-logotype fixed w-full top-0 z-1">
-    <div class="max-w-full h-19 mx-auto py-3 px-10 flex justify-between items-center">
-      <div class="flex items-center justify-between space-x-5">
-        <a class="text-2xl font-bold text-white flex items-center" href="/">
-          <Logo /><span class="block sm:hidden">Hack4Krak</span>
-        </a>
-        <div class="hidden space-x-10 sm:block">
-          <ULink
-            v-for="(url, text) in navbarItems" :key="text" :to="url"
-            class="text-white hover:underline"
-          >
-            {{ text }}
-          </ULink>
-        </div>
-      </div>
-      <button class="hidden sm:block px-3 py-2 mb-1 rounded-lg drop-shadow-[0_5px_#1A51AC] hover:drop-shadow-[0_5px_#6CA3FF] bg-[#EDB647] hover:bg-[#FDC657] font-pixelify text-xl text-blue-800 self-center cursor-pointer duration-300 transition-all font-bold">
-        <span>START GRY</span>
-      </button>
-      <button class="sm:hidden text-3xl flex items-center" @click="isExpanded = !isExpanded">
-        <Icon :name="isExpanded ? 'mdi:close' : 'mdi:hamburger-menu'" width="24" height="24" />
-      </button>
-    </div>
-    <div :class="isExpanded ? 'h-screen' : '-translate-y-[110%] h-0'" class="flex sm:hidden overflow-hidden flex-col justify-between duration-300 transition-all">
-      <div class="flex flex-col">
-        <ULink
-          v-for="(url, text) in navbarItems" :key="text" :to="url"
-          class="text-white hover:text-blue-500 p-3 text-2xl self-center"
-        >
-          {{ text }}
-        </ULink>
-      </div>
+  <UContainer class="top-0 z-1 sticky bg-white dark:bg-black max-w-full">
+    <div class="mx-auto flex items-center">
+      <NuxtLink to="/" class="flex items-center space-x-2">
+        <Logo class="size-10 text-black dark:text-white" />
+        <h1 class="md:hidden">
+          Hack4Krak
+        </h1>
+      </NuxtLink>
 
-      <button class="w-[50vw] px-2 mb-55 py-3 rounded-lg drop-shadow-[0_5px_#1A51AC] bg-[#EDB647] font-pixelify text-xl text-blue-800 self-center cursor-pointer font-bold">
-        <span>START GRY</span>
+      <!-- Desktop Navigation -->
+      <UNavigationMenu :items="items" class="hidden md:flex w-full" variant="link">
+        <template #button>
+          <ElevatedButton message="START GRY" />
+        </template>
+      </UNavigationMenu>
+
+      <button class="md:hidden p-2 ml-auto cursor-pointer" @click="toggleMobileMenu">
+        <Icon :name="isMobileMenuOpen ? 'i-lucide-menu' : 'i-lucide-minimize-2'" size="20" />
       </button>
     </div>
-  </nav>
+
+    <!-- Mobile Navigation -->
+    <transition
+      enter-from-class="opacity-0 translate-x-[100%]"
+      leave-to-class="opacity-0 translate-x-[100%]"
+      enter-active-class="transition duration-200"
+      leave-active-class="transition duration-200"
+    >
+      <div v-if="isMobileMenuOpen" class="md:hidden h-screen overflow-hidden">
+        <USeparator />
+        <UNavigationMenu
+          :items="items"
+          orientation="vertical"
+          variant="link"
+          class="w-full items-center justify-center text-center text-3xl"
+        >
+          <template #button>
+            <ElevatedButton message="START GRY" />
+          </template>
+        </UNavigationMenu>
+      </div>
+    </transition>
+  </UContainer>
 </template>
