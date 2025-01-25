c42164963db0e15f64f912b8e200a4a48e2f732b
norbiros <norbiros@protonmail.com>
2025-01-25 18:30:00 +0100

    fix: Properly update to `oauth` v5

--- backend/src/utils/app_state.rs
+++ backend/src/utils/app_state.rs
@@ -1,22 +1,34 @@
-use oauth2::basic::BasicClient;
-use oauth2::{AuthUrl, ClientId};
+use oauth2::basic::*;
+use oauth2::{
+    AuthUrl, Client, ClientId, EndpointNotSet, EndpointSet, RedirectUrl, StandardRevocableToken,
+    TokenUrl,
+};
 use sea_orm::DatabaseConnection;
 
 pub struct AppState {
     pub database: DatabaseConnection,
-    pub github_oauth_client: BasicClient,
+    pub github_oauth_client: Client<
+        BasicErrorResponse,
+        BasicTokenResponse,
+        BasicTokenIntrospectionResponse,
+        StandardRevocableToken,
+        BasicRevocationErrorResponse,
+        EndpointSet,
+        EndpointNotSet,
+        EndpointNotSet,
+        EndpointNotSet,
+        EndpointSet,
+    >,
 }
 
 impl AppState {
     pub fn with_database(database: DatabaseConnection) -> AppState {
         AppState {
             database,
-            github_oauth_client: BasicClient::new(
-                ClientId::new("test".to_string()),
-                None,
-                AuthUrl::new("http://auth".to_string()).unwrap(),
-                None,
-            ),
+            github_oauth_client: BasicClient::new(ClientId::new("test".to_string()))
+                .set_auth_uri(AuthUrl::new("https://authorize".to_string()).unwrap())
+                .set_token_uri(TokenUrl::new("https://token".to_string()).unwrap())
+                .set_redirect_uri(RedirectUrl::new("https://redirect".to_string()).unwrap()),
         }
     }
 }
