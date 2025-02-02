6506c4afdf41c307b61a416339468e2ebfada89e
Wojciech Piech <129212595+wojpo@users.noreply.github.com>
2025-02-02 17:11:54 +0100

    [Feat]: tasks page (#109)
    * tasks page
    
    * resolves requested changes
    
    * resolves requested changes
    
    * Rewrite functionality to use scrolling instead of position
    
    * update map image
    
    * refactor some magic numbers and export Element
    
    * improve alt
    
    * style refactors
    
    * use ref instead of getting object by id
    
    * scrolling via arrows on firefox (disgusting web browser)
    
    * auto focus on map and margin
    
    * lint fix

---
+++ frontend/app/components/Map.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+export interface Element {
+  x: number
+  y: number
+  content: string
+}
+
+defineProps<{
+  elements: Element[]
+}>()
+
+let isDragging = false
+let initialMousePositionX = 0
+let scrollLeft = 0
+const mapContainer = ref<HTMLElement | null>(null)
+
+const MAP_IMAGE_ASPECT_RATIO = 2550 / 480
+const IDEAL_VERTICAL_OVERFLOW_VALUE = 88
+
+function onMouseDown(event: MouseEvent) {
+  if (!mapContainer.value)
+    return
+  isDragging = true
+  initialMousePositionX = event.pageX
+  scrollLeft = mapContainer.value.scrollLeft
+}
+
+function onMouseMove(event: MouseEvent) {
+  if (!isDragging || !mapContainer.value)
+    return
+  event.preventDefault()
+  const currentMousePositionX = event.pageX
+  const dragDistanceX = (currentMousePositionX - initialMousePositionX) * -1
+  mapContainer.value.scrollLeft = scrollLeft + dragDistanceX
+}
+
+function onMouseUp() {
+  isDragging = false
+}
+</script>
+
+<template>
+  <div
+    ref="mapContainer"
+    autofocus
+    class="relative overflow-auto scrollbar-hide cursor-grab active:cursor-grabbing focus:outline-none"
+    @mousedown="onMouseDown"
+    @mousemove="onMouseMove"
+    @mouseup="onMouseUp"
+    @mouseleave="onMouseUp"
+  >
+    <div class="relative" :style="{ width: `${IDEAL_VERTICAL_OVERFLOW_VALUE * MAP_IMAGE_ASPECT_RATIO}vh` }">
+      <img
+        class="h-auto w-full object-cover rendering-pixelated select-none pointer-events-none"
+        src="/img/map.png"
+        alt="Map with tasks"
+      >
+      <div
+        v-for="(item, index) in elements"
+        :key="index"
+        class="absolute transform -translate-x-1/2 -translate-y-1/2"
+        :style="{ left: `${item.x}vh`, top: `${item.y}vh` }"
+      >
+        {{ item.content }}
+      </div>
+    </div>
+  </div>
+</template>
---
+++ frontend/app/pages/tasks.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { Element } from '@/components/Map.vue'
+import Map from '@/components/Map.vue'
+
+const elements = ref<Element[]>([
+  { x: 10, y: 20, content: 'Placeholder' },
+  { x: 30, y: 40, content: 'Placeholder' },
+  { x: 50, y: 60, content: 'Placeholder' },
+])
+</script>
+
+<template>
+  <Map :elements="elements" class="mt-1" />
+</template>
