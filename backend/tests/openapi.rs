8099111c66ae2a62e794f1d43126e77cc18faf25
kubaryt <kubastarzyk6554@gmail.com>
2025-02-12 21:28:55 +0100

    refactor: Replaced std::fs with tokio

--- backend/tests/openapi.rs
+++ backend/tests/openapi.rs
@@ -1,10 +1,12 @@
 use hack4krak_backend::setup_actix_app;
-use std::fs::read_to_string;
+use tokio::fs::read_to_string;
 
-#[test]
-fn test_openapi() {
+#[actix_web::test]
+async fn test_openapi() {
     let (_, api) = setup_actix_app().split_for_parts();
     let openapi = api.to_json().unwrap();
-    let generated_openapi = read_to_string("../frontend/openapi/api/openapi.json").unwrap();
+    let generated_openapi = read_to_string("../frontend/openapi/api/openapi.json")
+        .await
+        .unwrap();
     assert_eq!(openapi, generated_openapi);
 }
