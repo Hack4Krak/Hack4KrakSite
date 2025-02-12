e8d99cebd1fe6925cd67bd8887f32611ff818f2c
kubaryt <kubastarzyk6554@gmail.com>
2025-02-12 21:41:50 +0100

    refactor: Moved openapi check to bin crate and added it to workflow and pre-commit

--- backend/src/bin/openapi_check.rs
+++ backend/src/bin/openapi_check.rs
@@ -1,12 +1,13 @@
 use hack4krak_backend::setup_actix_app;
 use tokio::fs::read_to_string;
 
-#[actix_web::test]
-async fn test_openapi() {
+#[tokio::main]
+async fn main() {
     let (_, api) = setup_actix_app().split_for_parts();
     let openapi = api.to_json().unwrap();
     let generated_openapi = read_to_string("../frontend/openapi/api/openapi.json")
         .await
         .unwrap();
     assert_eq!(openapi, generated_openapi);
+    println!("OpenAPI specification is correct!");
 }
