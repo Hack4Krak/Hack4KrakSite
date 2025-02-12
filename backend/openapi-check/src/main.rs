b04346367091a73c50c1176bb535ec02630dd882
Kubaryt <kubastarzyk6554@gmail.com>
2025-02-12 23:07:05 +0100

    chore: Some code reformating and something something

--- backend/openapi-check/src/main.rs
+++ backend/openapi-check/src/main.rs
@@ -5,7 +5,7 @@ use tokio::fs::read_to_string;
 async fn main() {
     let (_, api) = setup_actix_app().split_for_parts();
     let openapi = api.to_json().unwrap();
-    let generated_openapi = read_to_string("../../frontend/openapi/api/openapi.json")
+    let generated_openapi = read_to_string("../frontend/openapi/api/openapi.json")
         .await
         .unwrap();
     assert_eq!(openapi, generated_openapi, "Your openapi json is...");
