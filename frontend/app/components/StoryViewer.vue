91e5dabb10b2d2a7884e8f587a3b72e4f064451d
wojpo <ilikeahottodogu@gmail.com>
2025-02-13 21:48:48 +0100

    resolve request changes (make it look like on figma, add some cool text animations, support arrows and space, support md and newlines)

--- frontend/app/components/StoryViewer.vue
+++ frontend/app/components/StoryViewer.vue
@@ -9,16 +9,56 @@ const props = defineProps<{
 const emit = defineEmits(['complete'])
 
 const characterIndex = ref(0)
-
+const displayedMessage = ref('')
 const currentCharacter = computed(() => props.storyDialogues[characterIndex.value] ?? { title: '', message: '' })
+
+let interval: NodeJS.Timeout
+
+function typeMessage(message: string) {
+  displayedMessage.value = message[0] ?? ' '
+  let index = 1
+  interval = setInterval(() => {
+    if (index < message.length) {
+      displayedMessage.value += message[index]
+      index++
+    } else {
+      clearInterval(interval)
+    }
+  }, 20)
+}
 
 function nextDialogue() {
   if (characterIndex.value < props.storyDialogues.length - 1) {
     characterIndex.value++
+    clearInterval(interval)
   } else {
     emit('complete')
   }
 }
+
+function prevDialogue() {
+  if (characterIndex.value > 0) {
+    characterIndex.value--
+    clearInterval(interval)
+  }
+}
+
+function handleKeydown(event: KeyboardEvent) {
+  if (event.key === 'ArrowRight' || event.key === ' ') {
+    nextDialogue()
+  } else if (event.key === 'ArrowLeft') {
+    prevDialogue()
+  }
+}
+
+watch(currentCharacter, (newCharacter) => {
+  typeMessage(newCharacter.message)
+})
+
+onMounted(() => {
+  window.addEventListener('keydown', handleKeydown)
+  typeMessage(currentCharacter.value.message)
+})
 </script>
 
 <template>
@@ -29,16 +69,14 @@ function nextDialogue() {
   >
     <div>
       <div class="relative w-full mx-auto p-4 bg-black/80 text-white shadow-lg text-center">
-        <div class="overflow-y-auto mb-15 sm:text-2xl md:text-3xl">
-          <p class="font-bold mb-5">
-            {{ currentCharacter.title || 'Unknown' }}
-          </p>
-          <div class="flex justify-center">
-            <div class="w-1/2 h-px bg-yellow-500 mb-10" />
+        <div class="overflow-y-auto mb-10 ">
+          <MDC :value="currentCharacter.title" class="font-extrabold text-2xl sm:text-3xl md:text-4xl mb-3" />
+          <div class="flex justify-center items-center mb-3">
+            <div class="w-[12px] h-[12px] bg-[#F6B216]" />
+            <div class="w-1/2 h-[2px] bg-[#F6B216]" />
+            <div class="w-[12px] h-[12px] bg-[#F6B216]" />
           </div>
-          <p>
-            {{ currentCharacter.message }}
-          </p>
+          <MDC :value="displayedMessage" class="text-xl md:text-2xl font-semibold" />
         </div>
       </div>
     </div>
