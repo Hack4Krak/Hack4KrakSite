71c8b8307dc7921b21dbec7e056f2ffad037daac
Kubaryt <88268063+Kubaryt@users.noreply.github.com>
2025-02-13 09:42:48 +0100

    Feat: Openapi check in workflows (#144)

---
+++ backend/openapi-check/src/main.rs
@@ -0,0 +1,13 @@
+use hack4krak_backend::setup_actix_app;
+use tokio::fs::read_to_string;
+
+#[tokio::main]
+async fn main() {
+    let (_, api) = setup_actix_app().split_for_parts();
+    let openapi = api.to_json().unwrap();
+    let generated_openapi = read_to_string("../frontend/openapi/api/openapi.json")
+        .await
+        .unwrap();
+    assert_eq!(openapi, generated_openapi, "Your openapi json is...");
+    println!("OpenAPI specification is correct!");
+}
