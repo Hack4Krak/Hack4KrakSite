4dd1b677a12117f1d209db2c8f5dfa26279bba75
kubaryt <kubastarzyk6554@gmail.com>
2025-02-14 15:39:18 +0100

    refactor: Changed not found function to closure

--- backend/src/utils/mod.rs
+++ backend/src/utils/mod.rs
@@ -2,7 +2,6 @@ pub mod app_state;
 pub mod cookies;
 pub mod error;
 pub mod jwt;
-pub mod not_found_error;
 pub mod oauth;
 pub mod openapi;
 pub mod success_response;
--- backend/src/utils/not_found_error.rs
+++
@@ -1,6 +0,0 @@
-use crate::utils::error::Error;
-use actix_web::{HttpResponse, ResponseError};
-
-pub async fn not_found() -> HttpResponse {
-    Error::RouteNotFound.error_response()
-}
