5eecf3c482b282779a24c7a83ec43621451230a5
Wojciech Piech <129212595+wojpo@users.noreply.github.com>
2025-02-15 14:06:49 +0100

    feat: Story viewer (#183)

---
+++ frontend/app/components/StoryViewer.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const props = defineProps<{
+  storyDialogues: { title: string, message: string }[]
+  image: string
+}>()
+
+const emit = defineEmits(['complete'])
+
+const phraseIndex = ref(0)
+const displayedPhrase = ref('')
+const currentMessage = computed(() => props.storyDialogues[phraseIndex.value] ?? { title: '', message: '' })
+
+let interval: NodeJS.Timeout | undefined
+
+function typeMessage(message: string) {
+  displayedPhrase.value = message[0] ?? ' '
+  let index = 1
+  interval = setInterval(() => {
+    if (index < message.length) {
+      displayedPhrase.value += message[index]
+      index++
+    } else {
+      clearInterval(interval)
+    }
+  }, 35)
+}
+
+function nextDialogue() {
+  if (phraseIndex.value < props.storyDialogues.length - 1) {
+    phraseIndex.value++
+    clearInterval(interval)
+  } else {
+    emit('complete')
+  }
+}
+
+function prevDialogue() {
+  if (phraseIndex.value > 0) {
+    phraseIndex.value--
+    clearInterval(interval)
+  }
+}
+
+function handleKeydown(event: KeyboardEvent) {
+  if (event.key === 'ArrowRight' || event.key === ' ') {
+    nextDialogue()
+  } else if (event.key === 'ArrowLeft') {
+    prevDialogue()
+  }
+}
+
+function handleClick(event: MouseEvent) {
+  if (event.clientX > window.innerWidth / 2) {
+    nextDialogue()
+  } else {
+    prevDialogue()
+  }
+}
+
+watch(currentMessage, (newCharacter) => {
+  typeMessage(newCharacter.message)
+})
+
+onMounted(() => {
+  window.addEventListener('keydown', handleKeydown)
+  typeMessage(currentMessage.value.message)
+})
+</script>
+
+<template>
+  <div
+    class="rendering-pixelated relative max-w-full max-h-screen justify-end flex flex-col bg-cover bg-center cursor-pointer"
+    :style="{ backgroundImage: `url('${props.image}')` }"
+    @click="handleClick"
+  >
+    <div>
+      <div class="relative w-full mx-auto p-4 bg-black/80 text-white shadow-lg text-center">
+        <div class="overflow-y-auto mb-10 ">
+          <MDC :value="currentMessage.title" class="font-extrabold text-2xl sm:text-3xl md:text-4xl mb-3" />
+          <div class="flex justify-center items-center mb-3">
+            <div class="w-[12px] h-[12px] bg-yellow-500" />
+            <div class="w-1/2 h-[2px] bg-yellow-500" />
+            <div class="w-[12px] h-[12px] bg-yellow-500" />
+          </div>
+          <MDC :value="displayedPhrase" class="text-xl md:text-2xl font-semibold" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ frontend/app/pages/tasks/story/[id].vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { FetchError } from 'ofetch'
+
+const route = useRoute()
+const taskId = String(route.params.id)
+
+const story = ref<{ title: string, message: string }[]>([])
+const backgroundImage = `${useRuntimeConfig().public.openFetch.api.baseURL}/tasks/background/${taskId}`
+
+function checkImage() {
+  const img = new Image()
+  img.src = backgroundImage
+  img.onerror = () => {
+    showError({
+      statusCode: 404,
+      message: 'Zdjecie nie zostalo znalezione',
+    })
+  }
+}
+
+try {
+  const { data: storyResponse } = await useApi('/tasks/story/{task_id}', {
+    path: { task_id: taskId },
+    key: `task-story-${taskId}`,
+  })
+
+  if (storyResponse.value === undefined) {
+    showError({
+      statusCode: 404,
+      message: 'Zadanie nie zostało znalezione',
+    })
+    console.error('Task not found')
+  }
+
+  if (storyResponse.value) {
+    story.value = storyResponse.value ?? []
+  }
+} catch (error) {
+  console.error(error)
+  if (!(error instanceof FetchError)) {
+    throw error
+  }
+
+  showError(error)
+}
+
+function redirectToTaskDescription() {
+  navigateTo(`/tasks/description/${taskId}`)
+}
+
+onMounted(() => {
+  checkImage()
+})
+</script>
+
+<template>
+  <StoryViewer :image="backgroundImage" :story-dialogues="story" @complete="redirectToTaskDescription" />
+</template>
