c55d207949c323980001f9dc20f4e1e502dc2a60
kubaryt <kubastarzyk6554@gmail.com>
2025-02-18 22:34:38 +0100

    feat: Tasks assets endpoints

---
+++ backend/src/routes/task/assets/list.rs
@@ -0,0 +1,24 @@
+use crate::utils::app_state;
+use crate::utils::error::Error;
+use actix_web::{get, web, HttpResponse};
+
+#[utoipa::path(
+    responses(
+        (status = 200, description = "List of task assets."),
+        (status = 404, description = "Task does not exist."),
+        (status = 500, description = "Internal server error.")
+    ),
+    security(
+        ("access_token" = [])
+    ),
+    tag = "task/assets"
+)]
+#[get("/list/{task_id}")]
+pub async fn list(
+    app_state: web::Data<app_state::AppState>,
+    task_id: web::Path<String>,
+) -> Result<HttpResponse, Error> {
+    let task = app_state.task_manager.get_task(&task_id.into_inner())?;
+
+    Ok(HttpResponse::Ok().json(&task.assets))
+}
