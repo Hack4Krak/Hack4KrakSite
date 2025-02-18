5c051d62cac840570c7fe3acf255168c63fbbd42
norbiros <norbiros@protonmail.com>
2025-02-18 23:09:16 +0100

    fix(frontend): Panel page important fixes

--- frontend/app/components/panel/Tile.vue
+++ frontend/app/components/panel/Tile.vue
@@ -3,9 +3,9 @@ import Placeholder from '~/components/primitive/Placeholder.vue'
 </script>
 
 <template>
-  <div class="relative tile bor tile border border-neutral-700 rounded-3xl">
+  <div class="relative tile bor tile border border-neutral-700 bg-(--ui-bg) rounded-3xl">
     <div class="overflow-hidden w-full h-full">
-      <div class="h-full rounded-3xl overflow-scroll">
+      <div class="h-full rounded-3xl">
         <slot>
           <Placeholder class="w-full h-full" />
         </slot>
