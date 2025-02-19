540c75a32648b411ec4e59861851bb6370ce928c
norbiros <norbiros@protonmail.com>
2025-02-19 16:47:55 +0100

    fix(frontend): More panel page fixes

--- frontend/app/components/panel/Tile.vue
+++ frontend/app/components/panel/Tile.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import Placeholder from '~/components/primitive/Placeholder.vue'
-</script>
-
 <template>
-  <div class="relative tile bor tile border border-neutral-700 bg-(--ui-bg) rounded-3xl">
+  <div class="relative tile bor tile border border-neutral-600 bg-(--ui-bg) rounded-3xl">
     <div class="overflow-hidden w-full h-full">
       <div class="h-full rounded-3xl">
         <slot>
--- frontend/app/components/panel/tile/Main.vue
+++ frontend/app/components/panel/tile/Main.vue
@@ -3,17 +3,15 @@
 </script>
 
 <template>
-  <div class="flex flex-col m-5 gap-5">
+  <div class="flex flex-col m-5 md:m-10 gap-5">
     <h2 class="text-2xl font-bold">
       Czas do początku wydarzenia
     </h2>
-    <span class="w-full text-center text-5xl font-bold">21 : 37 : 69</span>
-  </div>
-  <div class="flex flex-col m-5 gap-5">
-    <h2 class="text-2xl font-bold">
+    <span class="w-full text-center text-6xl font-bold">21 : 37 : 69</span>
+    <h2 class="text-2xl font-bold mt-5">
       Wyślij flagę
     </h2>
-    <UInput :ui="{ base: 'h-10' }" placeholder="hack4KrakCTF{...}" :disabled="true" />
+    <UInput :ui="{ base: 'h-12' }" placeholder="hack4KrakCTF{...}" :disabled="true" />
     <ElevatedButton message="Wyślij" class="w-40" />
   </div>
 </template>
