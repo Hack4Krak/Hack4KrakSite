a2ad7f87e7113dc9c3ff814cce4d104727c4c7a8
Norbiros <norbiros@protonmail.com>
2025-02-20 15:37:26 +0100

    feat(frontend): Basic stuff on panel page (#193)

---
+++ frontend/app/components/panel/tile/WithoutTeam.vue
@@ -0,0 +1,7 @@
+<template>
+  <PanelKing />
+  <div class="absolute bottom-10 w-full flex flex-col gap-5 z-1">
+    <ElevatedButton message="Utwórz zespół" class="w-70 h-15" />
+    <ElevatedButton message="Zaproszenia" class="w-70 h-15" />
+  </div>
+</template>
