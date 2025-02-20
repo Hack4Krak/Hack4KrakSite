1228c68dce91a67e8947d109b72a1bee2430c235
norbiros <norbiros@protonmail.com>
2025-02-20 17:12:06 +0100

    feat(frontend): Accept invites, kick users etc.
    # Conflicts:
    #	frontend/openapi/api/openapi.json

--- frontend/app/components/panel/modal/ViewInvitations.vue
+++ frontend/app/components/panel/modal/ViewInvitations.vue
@@ -1,9 +1,33 @@
 <script setup lang="ts">
-const { data } = await useAuth('/teams/invitations/', {
+const { data, refresh } = await useAuth('/teams/invitations/', {
   key: 'teams-invitations',
 })
 
 const open = defineModel<boolean>()
+
+watch(open, (newValue) => {
+  if (newValue) {
+    refresh()
+  }
+})
+
+async function accept(team_name: string) {
+  const { error } = await useAuth('/teams/invitations/accept_invitation/{team_name}', {
+    key: 'accept-invitation',
+    method: 'POST',
+    path: { team_name },
+  })
+
+  const toast = useToast()
+
+  if (error.value?.data) {
+    const response = error.value.data as any
+    toast.add({ title: 'Błąd', description: response.message, color: 'error' })
+  } else {
+    toast.add({ title: 'Sukces', description: 'Pomyślnie zaakceptowano użytkownika', color: 'success' })
+    await navigateTo('/panel/team')
+  }
+}
 </script>
 
 <template>
@@ -12,9 +36,10 @@ const open = defineModel<boolean>()
       <div v-if="data?.length === 0">
         Brak danych do wyświetlenia
       </div>
-      <div v-for="d in data" :key="d">
-        Unimplemented:
-        {{ d }}
+      <div v-for="team in data" :key="team">
+        {{ team }} <UButton @click="accept(team)">
+          Zaakceptuj
+        </UButton>
       </div>
     </template>
 
--- frontend/app/pages/panel/team.vue
+++ frontend/app/pages/panel/team.vue
@@ -1,16 +1,40 @@
 <script setup lang="ts">
-const { data: team } = await useAuth('/teams/membership/my_team', {
+const { data: team, refresh } = await useAuth('/teams/membership/my_team', {
   method: 'GET',
 })
+
+const inviteUserModal = ref(false)
 
 const members = computed(() => {
   const teamMembers = team?.value?.members || []
   return [...teamMembers, ...Array.from({ length: 5 }).fill(null)].slice(0, 5)
 })
+
+async function kick(username: string) {
+  const { error } = await useAuth('/teams/management/kick_user', {
+    key: 'kick-user',
+    method: 'DELETE',
+    body: {
+      username,
+    },
+  })
+
+  const toast = useToast()
+
+  if (error.value?.data) {
+    const response = error.value.data as any
+    toast.add({ title: 'Błąd', description: response.message, color: 'error' })
+  } else {
+    toast.add({ title: 'Sukces', description: 'Pomyślnie wyrzucono użytkownika', color: 'success' })
+    await refresh()
+  }
+}
 </script>
 
 <template>
   <div>
+    <PanelModalInviteUser v-model="inviteUserModal" />
+
     <NuxtLink to="/panel/">
       Wróć
     </NuxtLink>
@@ -24,13 +48,21 @@ const members = computed(() => {
         </UButton>
       </div>
       <div class="border-2 border-neutral-600 flex-grow rounded-2xl">
-        <div v-for="(user, index) in members" :key="index" class="border-b-1 border-neutral-600 last-of-type:border-0 p-5">
+        <div
+          v-for="(user, index) in members" :key="index"
+          class="border-b-1 border-neutral-600 last-of-type:border-0 p-5"
+        >
           <div v-if="user">
-            {{ user }}
-            <UButton>Remove</UButton>
+            <UIcon v-if="user.is_leader" name="i-material-symbols-crown" class="text-yellow-400" />
+            {{ user.name }}
+            <UButton @click="kick(user.name)">
+              Wyrzuć
+            </UButton>
           </div>
           <div v-else>
-            <UButton>Dodaj</UButton>
+            <UButton @click="inviteUserModal = true">
+              Dodaj
+            </UButton>
             Dodaj do zespołu
           </div>
         </div>
