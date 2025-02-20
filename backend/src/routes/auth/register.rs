185ec0ea25f631cbd47a1199a06793c65eb550db
kubaryt <kubastarzyk6554@gmail.com>
2025-02-20 17:04:21 +0100

    refactor: Switched to actix-web-validation crate

--- backend/src/routes/auth/register.rs
+++ backend/src/routes/auth/register.rs
@@ -1,12 +1,14 @@
 use crate::services::auth::AuthService;
 use crate::utils::app_state;
 use crate::utils::error::Error;
+use actix_web::web::Json;
 use actix_web::{post, web, HttpResponse};
+use actix_web_validation::Validated;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 use validator::Validate;
 
-#[derive(Serialize, Deserialize, ToSchema, Validate)]
+#[derive(Debug, Serialize, Deserialize, ToSchema, Validate)]
 pub struct RegisterModel {
     #[validate(length(min = 3, max = 32))]
     pub name: String,
@@ -27,7 +29,7 @@ pub struct RegisterModel {
 #[post("/register")]
 pub async fn register(
     app_state: web::Data<app_state::AppState>,
-    model: actix_web_validator::Json<RegisterModel>,
+    Validated(model): Validated<Json<RegisterModel>>,
 ) -> Result<HttpResponse, Error> {
     AuthService::register_with_password(&app_state, model.into_inner()).await
 }
--- backend/src/routes/teams/create.rs
+++ backend/src/routes/teams/create.rs
@@ -1,5 +1,7 @@
 use crate::routes::teams::AuthMiddleware;
+use actix_web::web::Json;
 use actix_web::{post, web, HttpResponse};
+use actix_web_validation::Validated;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 use validator::Validate;
@@ -10,7 +12,7 @@ use crate::utils::app_state;
 use crate::utils::error::Error;
 use crate::utils::success_response::SuccessResponse;
 
-#[derive(Serialize, Deserialize, ToSchema, Validate)]
+#[derive(Debug, Serialize, Deserialize, ToSchema, Validate)]
 pub struct CreateTeamModel {
     #[validate(length(min = 3, max = 32))]
     pub team_name: String,
@@ -32,7 +34,7 @@ pub struct CreateTeamModel {
 #[post("/create", wrap = "AuthMiddleware::with_user()")]
 pub async fn create(
     app_state: web::Data<app_state::AppState>,
-    create_team_model: actix_web_validator::Json<CreateTeamModel>,
+    Validated(create_team_model): Validated<Json<CreateTeamModel>>,
     user: users::Model,
 ) -> Result<HttpResponse, Error> {
     if let Some(team) = user.get_team(&app_state.database).await? {
