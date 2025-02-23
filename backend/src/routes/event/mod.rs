5a9d91cebe5cb3230e32355e6811e0167d4c0edf
kubaryt <kubastarzyk6554@gmail.com>
2025-02-23 14:54:02 +0100

    feat: Added is event in progress endpoint

--- backend/src/routes/event/mod.rs
+++ backend/src/routes/event/mod.rs
@@ -1,5 +1,7 @@
 mod info;
+mod is_event_in_progress;
 
 pub fn config(cfg: &mut utoipa_actix_web::service_config::ServiceConfig) {
     cfg.service(info::info);
+    cfg.service(is_event_in_progress::is_event_in_progress);
 }
