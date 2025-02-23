a4d669edc5723583a757f38b3e90ee49f6680469
kubaryt <kubastarzyk6554@gmail.com>
2025-02-23 15:00:02 +0100

    enhancement: Better error handling

--- frontend/app/middleware/tasks.global.ts
+++ frontend/app/middleware/tasks.global.ts
@@ -1,17 +1,27 @@
 export default defineNuxtRouteMiddleware(async (to) => {
   if (to.path.startsWith('/tasks')) {
     const toast = useToast()
+    let description = 'Nieznany błąd'
+
     try {
-      const response = await useApi('/tasks/list', {
+      const response = await useApi('/event/is_event_in_progress', {
         key: 'tasks',
         redirect: 'error',
       })
-      if (response.error.value) {
-        toast.add({ title: 'Błąd', description: 'Nie możesz otworzyć strony z zadaniami przed rozpoczęciem wydarzenia', color: 'error' })
+
+      const error = response.error.value
+      if (error) {
+        if (error.data.code === 403) {
+          description = 'Nie możesz otworzyć tej strony przed rozpocząciem wydarzenia'
+        } else if (error.data.code === 410) {
+          description = 'Wydarzenie zostało zakończone'
+        }
+
+        toast.add({ title: `Błąd ${error.data.code}`, description: `${description}`, color: 'error' })
         return '/'
       }
     } catch {
-      toast.add({ title: 'Błąd', description: 'Nie możesz otworzyć strony z zadaniami przed rozpoczęciem wydarzenia', color: 'error' })
+      toast.add({ title: 'Błąd', description: `${description}`, color: 'error' })
       return '/'
     }
   }
