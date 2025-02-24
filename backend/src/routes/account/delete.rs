92476e81df7f7f852ad32e063b83c52a948d08f2
kubaryt <kubastarzyk6554@gmail.com>
2025-02-24 19:01:18 +0100

    feat: Added account management backend

---
+++ backend/src/routes/account/delete.rs
@@ -0,0 +1,27 @@
+use crate::entities::users;
+use crate::middlewares::auth::AuthMiddleware;
+use crate::utils::app_state;
+use crate::utils::error::Error;
+use actix_web::web::Data;
+use actix_web::{HttpResponse, delete};
+use sea_orm::ModelTrait;
+
+#[utoipa::path(
+    responses(
+        (status = 200, description = "Account successfully deleted"),
+        (status = 500, description = "Internal server error.")
+    ),
+    security(
+        ("access_token" = [])
+    ),
+    tag = "account"
+)]
+#[delete("/delete", wrap = "AuthMiddleware::with_user()")]
+pub async fn delete(
+    app_state: Data<app_state::AppState>,
+    user: users::Model,
+) -> Result<HttpResponse, Error> {
+    user.delete(&app_state.database).await?;
+
+    Ok(HttpResponse::Ok().finish())
+}
