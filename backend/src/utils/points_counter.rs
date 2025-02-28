08b1502b7dae3a25e8c309ca5769e2d6720ec2c3
Kubaryt <kubastarzyk6554@gmail.com>
2025-02-28 11:41:11 +0100

    enhancement: Added captured flags number to leaderboard

--- backend/src/utils/points_counter.rs
+++ backend/src/utils/points_counter.rs
@@ -18,6 +18,13 @@ pub struct TeamPoints {
 pub struct TeamCurrentPoints {
     pub team_name: String,
     pub current_points: usize,
+    pub captured_flags: usize,
+}
+
+#[derive(Serialize, Deserialize, ToSchema, Debug, Default)]
+pub struct TeamPointsAndFlagsOverTime {
+    pub points: Vec<usize>,
+    pub flags: Vec<usize>,
 }
 
 #[derive(Serialize, Deserialize, ToSchema, Default, Debug, PartialEq)]
@@ -29,7 +36,7 @@ pub struct Chart {
 #[derive(Default)]
 pub struct PointsCounter {
     event_timestamps: Vec<NaiveDateTime>,
-    team_points_over_time: HashMap<String, Vec<usize>>,
+    team_points_and_flags_over_time: HashMap<String, TeamPointsAndFlagsOverTime>,
 }
 
 impl PointsCounter {
@@ -73,10 +80,18 @@ impl PointsCounter {
                 }
 
                 output
-                    .team_points_over_time
+                    .team_points_and_flags_over_time
                     .entry(team.name.clone())
                     .or_default()
+                    .points
                     .push(sum);
+
+                output
+                    .team_points_and_flags_over_time
+                    .entry(team.name.clone())
+                    .or_default()
+                    .flags
+                    .push(solves.unwrap_or(&Vec::new()).len());
             }
 
             output.event_timestamps.push(capture.submitted_at);
@@ -87,13 +102,15 @@ impl PointsCounter {
 
     pub fn get_final_team_points(&self) -> Vec<TeamCurrentPoints> {
         let mut points = self
-            .team_points_over_time
+            .team_points_and_flags_over_time
             .iter()
-            .map(|(team_name, points)| {
-                let final_points = *points.last().unwrap_or(&0);
+            .map(|(team_name, points_and_flags)| {
+                let final_points = *points_and_flags.points.last().unwrap_or(&0);
+                let final_flags = *points_and_flags.flags.last().unwrap_or(&0);
                 TeamCurrentPoints {
                     team_name: team_name.clone(),
                     current_points: final_points,
+                    captured_flags: final_flags,
                 }
             })
             .collect::<Vec<TeamCurrentPoints>>();
@@ -106,11 +123,11 @@ impl PointsCounter {
 
     pub fn to_chart(self) -> Chart {
         let points = self
-            .team_points_over_time
+            .team_points_and_flags_over_time
             .into_iter()
-            .map(|(team_id, time_points)| TeamPoints {
+            .map(|(team_id, time_points_and_flags)| TeamPoints {
                 label: team_id,
-                points: time_points,
+                points: time_points_and_flags.points,
             })
             .collect::<Vec<TeamPoints>>();
 
@@ -147,12 +164,12 @@ impl Serialize for PointsCounter {
         state.serialize_field("events", &self.event_timestamps)?;
 
         let team_points: Vec<_> = self
-            .team_points_over_time
+            .team_points_and_flags_over_time
             .iter()
-            .map(|(team_name, points)| {
+            .map(|(team_name, points_and_flags)| {
                 serde_json::json!({
                     "label": team_name,
-                    "data": points,
+                    "data": points_and_flags.points,
                 })
             })
             .collect();
