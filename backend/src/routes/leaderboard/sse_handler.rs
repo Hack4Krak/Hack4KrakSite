f5dd80c62c69fd7e2b6218479484fe6cc205bb99
kubaryt <kubastarzyk6554@gmail.com>
2025-03-05 21:17:20 +0100

    death…

--- backend/src/routes/leaderboard/sse_handler.rs
+++ backend/src/routes/leaderboard/sse_handler.rs
@@ -7,13 +7,17 @@ pub async fn sse_handler(app_state: web::Data<app_state::AppState>) -> impl Resp
     let tx = &app_state.leaderboard_updates_transmitter;
     let mut rx = tx.subscribe();
 
+    println!("SSE connection established");
+
     let server_events = stream! {
         while let Ok(msg) = rx.recv().await {
+            println!("Sending message: {}", msg);
             let data = format!("{}\n\n", msg);
             yield Ok::<Bytes, actix_web::Error>(web::Bytes::from(data));
         }
     };
 
+    println!("SSE connection closed");
     HttpResponse::Ok()
         .content_type("text/event-stream")
         .streaming(server_events)
