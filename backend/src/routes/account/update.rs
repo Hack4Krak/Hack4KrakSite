ffb78f79abc41257d4960116d5d548c127992dd3
kubaryt <kubastarzyk6554@gmail.com>
2025-03-20 18:58:41 +0100

    refactor: Remove pub(crate) from passwords in structs and implement debug for them without password fields

--- backend/src/routes/account/update.rs
+++ backend/src/routes/account/update.rs
@@ -8,15 +8,24 @@ use actix_web::web::{Data, Json};
 use actix_web::{HttpResponse, patch};
 use actix_web_validation::Validated;
 use serde::{Deserialize, Serialize};
+use std::fmt;
 use utoipa::ToSchema;
 use validator::Validate;
 
-#[derive(Debug, Serialize, Deserialize, ToSchema, Validate, Clone)]
+#[derive(Serialize, Deserialize, ToSchema, Validate, Clone)]
 pub struct UpdateUserModel {
     #[validate(length(min = 3, max = 32))]
     pub username: Option<String>,
-    pub(crate) old_password: String,
-    pub(crate) new_password: Option<String>,
+    pub old_password: String,
+    pub new_password: Option<String>,
+}
+
+impl fmt::Debug for UpdateUserModel {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("UpdateUserModel")
+            .field("username", &self.username)
+            .finish()
+    }
 }
 
 #[utoipa::path(
