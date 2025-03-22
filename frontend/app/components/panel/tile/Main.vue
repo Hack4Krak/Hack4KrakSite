fc2ce0d8c405c2741fe8236181e049ba8107bbfd
norbiros <norbiros@protonmail.com>
2025-03-22 11:09:33 +0100

    fix(frontend): Manually extend dayjs

--- frontend/app/components/panel/tile/Main.vue
+++ frontend/app/components/panel/tile/Main.vue
@@ -1,5 +1,7 @@
 <script setup>
-const dayjs = useDayjs()
+import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration'
+import isBetween from 'dayjs/plugin/isBetween'
 
 const { data } = await useAuth('/event/info')
 
@@ -7,10 +9,13 @@ const { data: timeLeft } = useAsyncData('timeLeft', async () => calculateTimeLef
 const eventMessage = computed(() => getEventState().message)
 
 function getEventState() {
+  // Temporary solution for https://github.com/fumeapp/dayjs/issues/62
+  dayjs.extend(duration)
+  dayjs.extend(isBetween)
   const now = dayjs()
   const eventStart = dayjs(data.value?.start_date)
-
   const eventEnd = dayjs(data.value?.end_date)
+
   if (now.isBetween(eventStart, eventEnd)) {
     return { message: 'Czas do zakończenia wydarzenia', diff: dayjs.duration(eventEnd.diff()) }
   } else if (now.isBefore(eventStart)) {
