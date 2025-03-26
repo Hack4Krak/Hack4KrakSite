3177cea1016bdd25f6c526217b964b035db42c41
kubaryt <kubastarzyk6554@gmail.com>
2025-03-26 17:53:29 +0100

    refactor(backend): Made some modules private

--- backend/src/models/mod.rs
+++ backend/src/models/mod.rs
@@ -1,8 +1,8 @@
 mod email_confirmation;
-pub mod flag_capture;
+mod flag_capture;
 pub mod task;
-pub mod team_invites;
+mod team_invites;
 mod team_status;
-pub mod teams;
+mod teams;
 pub mod user;
 mod user_roles;
