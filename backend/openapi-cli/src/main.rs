3ccbe1560343e78bb28f63a98a4f380f695ee8cb
kubaryt <kubastarzyk6554@gmail.com>
2025-03-31 19:04:49 +0200

    refactor: Load openapi path from env

--- backend/openapi-cli/src/main.rs
+++ backend/openapi-cli/src/main.rs
@@ -1,8 +1,11 @@
+use hack4krak_backend::services::env::EnvConfig;
 use hack4krak_backend::setup_actix_app;
 use tokio::fs::read_to_string;
 
 #[tokio::main]
 async fn main() {
+    EnvConfig::load_config();
+
     let (_, api) = setup_actix_app(false).split_for_parts();
     let openapi = api.to_json().unwrap();
     let args = std::env::args().collect::<Vec<String>>();
@@ -14,7 +17,7 @@ async fn main() {
 }
 
 async fn verify_openapi(openapi: String) {
-    let generated_openapi = read_to_string("../frontend/openapi/api/openapi.json")
+    let generated_openapi = read_to_string(&EnvConfig::get().openapi_json_frontend_path)
         .await
         .unwrap();
     assert_eq!(openapi, generated_openapi, "Your openapi json is...");
@@ -22,7 +25,7 @@ async fn verify_openapi(openapi: String) {
 }
 
 async fn write_openapi(openapi: String) {
-    tokio::fs::write("../frontend/openapi/api/openapi.json", openapi)
+    tokio::fs::write(&EnvConfig::get().openapi_json_frontend_path, openapi)
         .await
         .unwrap();
     println!("OpenAPI specification is written to frontend/openapi/api/openapi.json");
