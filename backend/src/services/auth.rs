8ff7b26eb6d99d5760bd7d8142f2afb5720e6464
kubaryt <kubastarzyk6554@gmail.com>
2025-04-02 19:29:29 +0200

    refactor: Changed push_str to format!

--- backend/src/services/auth.rs
+++ backend/src/services/auth.rs
@@ -44,8 +44,7 @@ impl AuthService {
 
         let confirmation_link = Self::create_email_confirmation_link(&confirmation_code)?;
 
-        let mut sender_email = "auth@".to_string();
-        sender_email.push_str(&EnvConfig::get().domain_name);
+        let sender_email = format!("auth@{}", &EnvConfig::get().domain_name);
 
         Email {
             sender: (Some("Autoryzacja Hack4Krak".to_string()), sender_email),
