52b03ac028c951703706bbd0be682d6f8d2d8b1e
norbiros <norbiros@protonmail.com>
2025-04-10 18:24:26 +0200

    feat(frontend): Include icons in `package.json` (#274)

--- frontend/app/components/panel/tile/WithTeam.vue
+++ frontend/app/components/panel/tile/WithTeam.vue
@@ -36,7 +36,7 @@ const flagsLeft = computed(
       Nazwa zespołu: <span class="text-(--ui-primary)">{{ props.teamName }}</span>
     </h1>
     <NuxtLink to="/panel/team" class="flex items-center gap-5 border-neutral-800 border-2 rounded-2xl px-5 py-2 cursor-pointer hover:bg-neutral-800 transition-all duration-150">
-      <UIcon name="i-fluent-people-team-16-filled" class="size-15" />
+      <UIcon name="mdi:account-group" class="size-15" />
       <h3 class="font-semibold">
         Panel zespołu
       </h3>
