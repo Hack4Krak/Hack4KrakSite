cfded72a0bf4fa30c0f996d7bebb9bd6203c40ed
norbiros <norbiros@protonmail.com>
2025-04-13 13:12:37 +0200

    feat(frontend): Implemented some basic tests

--- frontend/tests/e2e/basic.test.ts
+++ frontend/tests/e2e/basic.test.ts
@@ -1,6 +1,16 @@
 import { expect, test } from '@nuxt/test-utils/playwright'
 
-test('Webpage content loading', async ({ page }) => {
+test('webpage basic content loading', async ({ page }) => {
   await page.goto('/')
+
+  // Check heading
   await expect(page.getByRole('heading')).toContainText('Hack4Krak')
+
+  // Check title
+  await expect(page).toHaveTitle('Strona Główna | Hack4Krak CTF')
+
+  // Check favicon
+  const favicon = await page.$('link[rel="icon"]')
+  const href = await favicon?.getAttribute('href')
+  expect(href).toContain('favicon')
 })
