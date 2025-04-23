0e233ffefbdfa3e360f63f4dce03311581630b2b
wikipop <66514885+wikipop@users.noreply.github.com>
2025-04-23 23:59:16 +0200

    fix: panel ui

--- frontend/app/components/panel/tile/WithoutTeam.vue
+++ frontend/app/components/panel/tile/WithoutTeam.vue
@@ -8,11 +8,11 @@ const invitationsModal = ref(false)
   <PanelModalViewInvitations v-model="invitationsModal" />
 
   <PanelKing />
-  <div class="absolute bottom-10 w-full flex flex-col gap-5 z-1">
-    <ElevatedButton class="w-70 h-15" @click="createTeamModal = true">
+  <div class="ml-12 absolute bottom-10 w-full flex flex-col gap-5 z-1">
+    <ElevatedButton class="w-70" @click="createTeamModal = true">
       Utwórz zespół
     </ElevatedButton>
-    <ElevatedButton class="w-70 h-15" @click="invitationsModal = true">
+    <ElevatedButton class="w-70" @click="invitationsModal = true">
       Zaproszenia
     </ElevatedButton>
   </div>
