7eb77520be73821289b9a0eeec601099434f9782
norbiros <norbiros@protonmail.com>
2025-04-26 20:27:47 +0200

    chore(frontend): Use `mdi` icon pack

--- frontend/app/pages/panel/team.vue
+++ frontend/app/pages/panel/team.vue
@@ -101,7 +101,7 @@ const members = computed<Members | null>(() => {
           >
             <div v-if="user" class="flex justify-between items-center">
               <div>
-                <UIcon v-if="user.is_leader" name="i-material-symbols-crown" class="text-yellow-400" />
+                <UIcon v-if="user.is_leader" name="mdi:crown" class="text-yellow-400" />
                 {{ user.name }}
               </div>
               <UButton v-if="is_user_leader" @click="kickUserModal = true; kickedUser = user.name">
