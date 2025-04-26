09565d9e0d34ef535fc96d71d5263237edd79313
wikipop <66514885+wikipop@users.noreply.github.com>
2025-04-26 19:35:15 +0200

    feat: implemented nuxt content

--- frontend/app/pages/rules.vue
+++ frontend/app/pages/rules.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import file from '~/content/docs/School_Edition_Guidelines.md?raw'
+// Fallback
+definePageMeta({
+  middleware: () => {
+    return navigateTo('/docs/rules')
+  },
+})
 </script>
 
 <template>
-  <div class="w-full mt-5 md:pl-[25svw] pl-[15svw]">
-    <MarkdownContent :text="file" class="md:w-[50svw] w-[70svw]" prose="dark" />
-  </div>
+  <!-- Content will be rendered by the dynamic route -->
 </template>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     '@nuxt/eslint',
     '@nuxt/image',
+    '@nuxt/content',
     '@nuxt/test-utils/module',
     '@nuxtjs/seo',
     '@nuxtjs/mdc',
@@ -36,6 +37,7 @@ export default defineNuxtConfig({
     '/tasks/description/**': { swr: true },
     '/faq': { prerender: true },
     '/rules': { prerender: true },
+    '/docs/**': { prerender: true },
     '/': { prerender: true },
   },
   app: {
