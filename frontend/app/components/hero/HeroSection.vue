3ead231a5924b80d166812504e572a4c20ce216e
wikipop <66514885+wikipop@users.noreply.github.com>
2025-05-01 21:44:09 +0200

    refactor: update hero section component imports and usage to use prefixed names for consistency

--- frontend/app/components/hero/HeroSection.vue
+++ frontend/app/components/hero/HeroSection.vue
@@ -1,17 +1,10 @@
-<script setup lang="ts">
-import CallToAction from '~/components/hero/CallToAction.vue'
-import DesktopHeroBackground from '~/components/hero/DesktopHeroBackground.vue'
-import MobileLogoSection from '~/components/hero/MobileLogoSection.vue'
-import SocialMediaContainer from '~/components/hero/SocialMediaContainer.vue'
-</script>
-
 <template>
   <section
     class="lg:h-[calc(100vh-var(--ui-header-height))] flex flex-col items-center
            w-full mb-12 gap-y-12 lg:gap-0"
   >
-    <DesktopHeroBackground hydrate-on-visible class="h-3/4" />
-    <MobileLogoSection hydrate-on-visible />
+    <HeroDesktopHeroBackground hydrate-on-visible class="h-3/4" />
+    <HeroMobileLogoSection hydrate-on-visible />
     <div
       class="flex-1 flex flex-col lg:flex-row items-center bg-transparent
              gap-y-12 lg:gap-0 place-content-between xl:w-(--ui-container)"
@@ -24,8 +17,8 @@ import SocialMediaContainer from '~/components/hero/SocialMediaContainer.vue'
           <slot name="content" />
         </template>
       </HeroContent>
-      <SocialMediaContainer />
+      <HeroSocialMediaContainer />
     </div>
-    <CallToAction hydrate-on-visible class="lg:hidden" />
+    <HeroCallToAction hydrate-on-visible class="lg:hidden" />
   </section>
 </template>
