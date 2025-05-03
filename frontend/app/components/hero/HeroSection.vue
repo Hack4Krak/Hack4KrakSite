295bf905cb128d957e447bd8b87993d447c90af0
Wiktor Popiołek <66514885+wikipop@users.noreply.github.com>
2025-05-03 12:10:28 +0200

    Revert "refactor: update hero section component imports and usage to use prefixed names for consistency"
    This reverts commit 3ead231a5924b80d166812504e572a4c20ce216e.

--- frontend/app/components/hero/HeroSection.vue
+++ frontend/app/components/hero/HeroSection.vue
@@ -1,10 +1,17 @@
+<script setup lang="ts">
+import CallToAction from '~/components/hero/CallToAction.vue'
+import DesktopHeroBackground from '~/components/hero/DesktopHeroBackground.vue'
+import MobileLogoSection from '~/components/hero/MobileLogoSection.vue'
+import SocialMediaContainer from '~/components/hero/SocialMediaContainer.vue'
+</script>
+
 <template>
   <section
     class="lg:h-[calc(100vh-var(--ui-header-height))] flex flex-col items-center
            w-full mb-12 gap-y-12 lg:gap-0"
   >
-    <HeroDesktopHeroBackground hydrate-on-visible class="h-3/4" />
-    <HeroMobileLogoSection hydrate-on-visible />
+    <DesktopHeroBackground hydrate-on-visible class="h-3/4" />
+    <MobileLogoSection hydrate-on-visible />
     <div
       class="flex-1 flex flex-col lg:flex-row items-center bg-transparent
              gap-y-12 lg:gap-0 place-content-between xl:w-(--ui-container)"
@@ -17,8 +24,8 @@
           <slot name="content" />
         </template>
       </HeroContent>
-      <HeroSocialMediaContainer />
+      <SocialMediaContainer />
     </div>
-    <HeroCallToAction hydrate-on-visible class="lg:hidden" />
+    <CallToAction hydrate-on-visible class="lg:hidden" />
   </section>
 </template>
