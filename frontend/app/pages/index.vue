21edf70b66a88426f5606c90b052614637304883
wikipop <66514885+wikipop@users.noreply.github.com>
2025-05-04 15:56:22 +0200

    feat(frontend): Added `how-to-participate` section

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -19,37 +19,40 @@ useSeoMeta({
       </template>
     </HeroSection>
     <UContainer class="w-full h-screen mb-24">
-      <DevOnly>
-        <section class="my-8">
-          <div class="flex flex-col border-green-400 border-2 bg-green-400/10 p-6 text-center justify-center">
-            <p class="text-xl lg:text-3xl font-bold text-green-400">
-              {{ LANDING_CONTENT.infoBox }}
-            </p>
-          </div>
-        </section>
-      </DevOnly>
-      <section class="info-section flex flex-col lg:grid grid-cols-3 gap-2 w-full auto-cols-auto">
-        <InfoTile
-          v-for="(info, index) in INFO_TILES_CONTENT"
-          :key="`tile-${index}`"
-          :content="info"
-        />
-        <div
-          class="col-span-2 border-red-450 border-2 flex flex-col px-4 py-12 text-center lg:text-left
-                 lg:flex-row justify-center gap-12 items-center"
-        >
-          <div class="">
-            <p class="lg:text-2xl font-bold text-red-450">
-              Chcesz wziąć udział?
-            </p> <br>
-            <NuxtLink to="/panel">
-              <ElevatedButton class="lg:text-xl lg:scale-110">
-                Zarejestruj swoją drużynę!
-              </ElevatedButton>
-            </NuxtLink>
-          </div>
-          <NuxtImg src="/img/character/dragon.png" class="rendering-pixelated w-32 lg:w-64" />
+      <section class="my-8">
+        <div class="flex flex-col gap-8 border-green-400 border-2 bg-green-400/10 p-6 text-center justify-center">
+          <p class="text-xl lg:text-3xl font-bold text-green-400">
+            {{ LANDING_CONTENT.infoBox }}
+          </p>
         </div>
+      </section>
+      <section class="my-8">
+        <div class="flex flex-col lg:grid grid-cols-3 gap-2 w-full auto-cols-auto">
+          <InfoTile
+            v-for="(info, index) in INFO_TILES_CONTENT"
+            :key="`tile-${index}`"
+            :content="info"
+          />
+          <div
+            class="col-span-2 border-red-450 border-2 flex flex-col px-4 py-12 text-center lg:text-left
+                 lg:flex-row justify-center gap-12 items-center"
+          >
+            <div class="">
+              <p class="lg:text-2xl font-bold text-red-450">
+                Chcesz wziąć udział?
+              </p> <br>
+              <NuxtLink to="/panel">
+                <ElevatedButton class="lg:text-xl lg:scale-110">
+                  Zarejestruj swoją drużynę!
+                </ElevatedButton>
+              </NuxtLink>
+            </div>
+            <NuxtImg src="/img/character/dragon.png" class="rendering-pixelated w-32 lg:w-64" />
+          </div>
+        </div>
+      </section>
+      <section id="section-how-to-participate" class="my-8">
+        <SectionHowToParticipate />
       </section>
       <Footer class="mt-8 lg:mt-12" />
     </UContainer>
