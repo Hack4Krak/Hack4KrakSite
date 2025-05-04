0cda80b81fff337dd6492efe5b403c273e4d8549
wikipop <66514885+wikipop@users.noreply.github.com>
2025-05-04 11:12:07 +0200

    feat: add size variants to LogoWithText component and implement in Footer component

--- frontend/app/components/LogoWithText.vue
+++ frontend/app/components/LogoWithText.vue
@@ -1,7 +1,39 @@
+<script setup lang="ts">
+import { tv } from 'tailwind-variants'
+
+const props = withDefaults(defineProps<{
+  size?: 'sm' | 'md' | 'lg'
+}>(), {
+  size: 'md',
+})
+
+const logoVariants = tv({
+  base: 'mr-4',
+  variants: {
+    size: {
+      sm: 'size-6',
+      md: 'size-9',
+      lg: 'size-12',
+    },
+  },
+})
+
+const pVariants = tv({
+  base: 'font-semibold font-pixelify',
+  variants: {
+    size: {
+      sm: 'text-lg',
+      md: 'text-xl',
+      lg: 'text-2xl',
+    },
+  },
+})
+</script>
+
 <template>
-  <div class="flex items-center">
-    <Logo class="size-9 text-white mr-4" />
-    <p class="font-pixelify text-xl font-semibold text-white">
+  <div class="flex items-center text-default">
+    <Logo :class="logoVariants({ size: props.size })" />
+    <p :class="pVariants({ size: props.size })">
       Hack4Krak
     </p>
   </div>
