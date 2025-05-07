9c4b36626989884088eab8b31b027159d0d2f84a
norbiros <norbiros@protonmail.com>
2025-05-07 15:40:29 +0200

    refactor(frontend): Extract `inline` option to `MarkdownContent`

--- frontend/app/components/primitive/OrderedList.vue
+++ frontend/app/components/primitive/OrderedList.vue
@@ -8,7 +8,7 @@ defineProps<{
   <ol class="lg:max-w-1/2 mx-auto mt-6 mb-2 flex flex-col text-left prose dark:prose-invert">
     <li v-for="(sentence, index) in items" :key="index">
       <span class="font-bold">{{ index + 1 }}. </span>
-      <MDC :value="sentence" class="text-red inline [&>p]:inline" />
+      <MarkdownContent :text="sentence" :inline="true" />
     </li>
   </ol>
 </template>
