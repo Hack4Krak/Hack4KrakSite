355b5f7248e9110c031cd1f3d5693152c373f9d1
wikipop <66514885+wikipop@users.noreply.github.com>
2025-05-08 15:34:10 +0200

    fix: repeating images on niche resolutions

--- frontend/app/components/hero/DesktopHeroBackground.vue
+++ frontend/app/components/hero/DesktopHeroBackground.vue
@@ -13,7 +13,7 @@ useHead({
 <template>
   <div class="-z-10 hidden lg:grid w-full place-items-center overflow-hidden">
     <div
-      class="bg-[url(/img/landing_background.webp)] rendering-pixelated bg-cover w-full h-full will-change-transform"
+      class="bg-[url(/img/landing_background.webp)] rendering-pixelated bg-cover bg-no-repeat w-full h-full will-change-transform"
       :style="{
         opacity: 1 - (y / 1500),
         backgroundPosition: `0 -${y / 6}px`,
