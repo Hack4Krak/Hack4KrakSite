4533d9e9ea99d1db4a7fb1ca6a4919b00a255d50
Kubaryt <kubastarzyk6554@gmail.com>
2025-05-15 15:08:04 +0200

    fix(frontend): Make Navbar have single root element

--- frontend/app/components/Navbar.server.vue
+++ frontend/app/components/Navbar.server.vue
@@ -24,62 +24,64 @@ watch(() => router.currentRoute.value, () => {
 </script>
 
 <template>
-  <DefineNavbarTemplate>
-    <UNavigationMenu
-      content-orientation="vertical"
-      :items="NAVBAR_ITEMS" variant="link" class="w-full" color="error"
-      :ui="{
-        linkLabel: 'hover:underline underline-offset-5',
-        viewport: 'w-(--reka-navigation-menu-viewport-width)',
-        childList: 'flex-col items-center',
-        link: 'text-md text-default data-active:text-primary',
-        list: 'gap-4',
-      }"
-    >
-      <template #logo>
-        <div class="md:flex hidden">
-          <LogoWithText />
-        </div>
-      </template>
-
-      <template #button>
-        <ElevatedButton>
-          {{ isLoggedIn ? "Otwórz panel" : "Zaloguj się!" }}
-        </ElevatedButton>
-      </template>
-    </UNavigationMenu>
-  </DefineNavbarTemplate>
-
-  <UContainer class="sticky top-0 max-w-full font-sans bg-default z-20 print:hidden">
-    <div class="hidden md:block h-(--ui-header-height)">
-      <ReuseNavbarTemplate />
-    </div>
-
-    <!-- Mobile Navigation -->
-    <div class="md:hidden flex pt-4">
-      <NuxtLink to="/" class="flex items-center mb-4">
-        <LogoWithText />
-      </NuxtLink>
-      <button
-        class="p-2 ml-auto cursor-pointer flex justify-center"
-        aria-label="Przełącz nawigacje mobilną"
-        data-testid="mobile-menu-toggle"
-        @click="toggleMobileMenu"
+  <div>
+    <DefineNavbarTemplate>
+      <UNavigationMenu
+        content-orientation="vertical"
+        :items="NAVBAR_ITEMS" variant="link" class="w-full" color="error"
+        :ui="{
+          linkLabel: 'hover:underline underline-offset-5',
+          viewport: 'w-(--reka-navigation-menu-viewport-width)',
+          childList: 'flex-col items-center',
+          link: 'text-md text-default data-active:text-primary',
+          list: 'gap-4',
+        }"
       >
-        <Icon :name="isMobileMenuOpen ? 'mdi:close' : 'mdi:hamburger-menu'" size="28" />
-      </button>
-    </div>
+        <template #logo>
+          <div class="md:flex hidden">
+            <LogoWithText />
+          </div>
+        </template>
 
-    <Transition
-      enter-from-class="opacity-0 translate-x-[100%]"
-      leave-to-class="opacity-0 translate-x-[100%]"
-      enter-active-class="transition duration-200"
-      leave-active-class="transition duration-200"
-      hydrate-on-media-query="(max-width: 768px)"
-    >
-      <div v-if="isMobileMenuOpen" class="-my-5 md:hidden h-screen [&>a]:text-5xl ">
-        <ReuseNavbarTemplate orientation="vertical" />
+        <template #button>
+          <ElevatedButton>
+            {{ isLoggedIn ? "Otwórz panel" : "Zaloguj się!" }}
+          </ElevatedButton>
+        </template>
+      </UNavigationMenu>
+    </DefineNavbarTemplate>
+
+    <UContainer class="sticky top-0 max-w-full font-sans bg-default z-20 print:hidden">
+      <div class="hidden md:block h-(--ui-header-height)">
+        <ReuseNavbarTemplate />
       </div>
-    </Transition>
-  </UContainer>
+
+      <!-- Mobile Navigation -->
+      <div class="md:hidden flex pt-4">
+        <NuxtLink to="/" class="flex items-center mb-4">
+          <LogoWithText />
+        </NuxtLink>
+        <button
+          class="p-2 ml-auto cursor-pointer flex justify-center"
+          aria-label="Przełącz nawigacje mobilną"
+          data-testid="mobile-menu-toggle"
+          @click="toggleMobileMenu"
+        >
+          <Icon :name="isMobileMenuOpen ? 'mdi:close' : 'mdi:hamburger-menu'" size="28" />
+        </button>
+      </div>
+
+      <Transition
+        enter-from-class="opacity-0 translate-x-[100%]"
+        leave-to-class="opacity-0 translate-x-[100%]"
+        enter-active-class="transition duration-200"
+        leave-active-class="transition duration-200"
+        hydrate-on-media-query="(max-width: 768px)"
+      >
+        <div v-if="isMobileMenuOpen" class="-my-5 md:hidden h-screen [&>a]:text-5xl ">
+          <ReuseNavbarTemplate orientation="vertical" />
+        </div>
+      </Transition>
+    </UContainer>
+  </div>
 </template>
