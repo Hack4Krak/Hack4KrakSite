e717a77b477c2ecf870d5b67b0ad292d6c90b188
Kubaryt <kubastarzyk6554@gmail.com>
2025-05-27 17:18:12 +0200

    wip i fucking hate openapi or utoipa i dont know what causes this mess

--- backend/src/routes/admin/team_invites/list.rs
+++ backend/src/routes/admin/team_invites/list.rs
@@ -1,11 +1,14 @@
 use crate::entities::{external_team_invitation, team_invites};
+use crate::routes::admin::team_invites::TeamInviteListResponse;
 use crate::utils::app_state::AppState;
 use crate::utils::error::Error;
 use actix_web::web::Data;
 use actix_web::{HttpResponse, get};
+use sea_orm::EntityTrait;
+
 #[utoipa::path(
     responses(
-        (status = 200, description = "Team invites successfully fetched.", body = Vec<team_invites::Model>),
+        (status = 200, description = "Team invites successfully fetched.", body = TeamInviteListResponse),
         (status = 500, description = "Internal server error.")
     ),
     operation_id = "admin_list_team_invites",
@@ -20,26 +23,12 @@ pub async fn list(app_state: Data<AppState>) -> Result<HttpResponse, Error> {
         .all(&app_state.database)
         .await?;
 
-    Ok(HttpResponse::Ok().json(team_invites))
-}
-
-use sea_orm::EntityTrait;
-
-#[utoipa::path(
-    responses(
-        (status = 200, description = "External team invites successfully fetched.", body = Vec<external_team_invitation::Model>),
-        (status = 500, description = "Internal server error.")
-    ),
-    security(
-        ("access_token" = [])
-    ),
-    tag = "admin/team_invites"
-)]
-#[get("/list_external")]
-pub async fn list_external(app_state: Data<AppState>) -> Result<HttpResponse, Error> {
     let external_team_invites = external_team_invitation::Entity::find()
         .all(&app_state.database)
         .await?;
 
-    Ok(HttpResponse::Ok().json(external_team_invites))
+    Ok(HttpResponse::Ok().json(TeamInviteListResponse {
+        team_invites,
+        external_team_invites,
+    }))
 }
