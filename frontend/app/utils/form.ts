28488f9704195a7b4abf06600fab048ed8ebd722
norbiros <norbiros@protonmail.com>
2025-07-17 17:34:57 +0200

    feat(frontend): add `zod` auto imports and utils

---
+++ frontend/app/utils/form.ts
@@ -0,0 +1,9 @@
+export function zPassword() {
+  return z.string({ error: 'Hasło jest wymagane' })
+    .min(8, 'Hasło musi mieć minimum 8 znaków')
+}
+
+export function zTeamName() {
+  return z.string({ error: 'Nazwa drużyny jest wymagana' })
+    .min(5, 'Nazwa drużyny musi mieć min 5 znaków')
+}
