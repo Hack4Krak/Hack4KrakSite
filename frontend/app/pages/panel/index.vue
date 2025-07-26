fc36b262a3ed7e5d72e620d90cf7623c1e92f220
norbiros <norbiros@protonmail.com>
2025-07-26 17:09:05 +0200

    refactor(frontend): reorganize panel related components

--- frontend/app/pages/panel/index.vue
+++ frontend/app/pages/panel/index.vue
@@ -32,7 +32,7 @@ useRafFn(() => updateDate())
   >
     <!-- Top full-width bar -->
     <div class="col-span-2 border-b h-15 flex items-center divide-x font-bold">
-      <span class="w-15 h-full flex items-center justify-center font-bold">
+      <span class="w-15 h-full flex items-center justify-center text-xl">
         X
       </span>
       <span v-if="team?.team_name" class="px-5 h-full flex items-center">
@@ -52,69 +52,19 @@ useRafFn(() => updateDate())
     <div v-if="team" class="row-span-3 p-4 flex flex-col justify-center space-y-2">
       <span class="font-bold">Moja drużyna</span>
       <USeparator :ui="{ border: 'border-neutral' }" />
-      <div v-if="(team?.members ?? []).length">
-        <div v-for="member in team?.members" :key="member.name">
-          {{ member.name }}
-        </div>
-      </div>
-      <div v-else>
-        Proszę stworzyć drużynę!
+      <div v-for="member in team?.members" :key="member.name">
+        {{ member.name }}
       </div>
     </div>
 
     <!-- Top two boxes -->
     <div class="flex divide-x border-b font-pixelify" :class="{ 'col-span-2': !team }">
-      <div class="flex-1  shadow items-center justify-center flex-col flex">
-        <PanelFlagForm />
+      <div class="flex flex-1 flex-col shadow items-center justify-center">
+        <PanelTileFlagForm />
       </div>
-      <div class="w-2/5 flex flex-col gap-5 items-center justify-center overflow-clip">
-        <h3 class="text-xl font-bold">
-          Ważne linki
-        </h3>
-        <NuxtLink to="/tasks">
-          <ElevatedButton class="w-60">
-            Zadania
-          </ElevatedButton>
-        </NuxtLink>
-        <NuxtLink to="/leaderboard">
-          <ElevatedButton class="w-60">
-            Ranking
-          </ElevatedButton>
-        </NuxtLink>
-        <NuxtLink to="/panel/profile">
-          <ElevatedButton class="w-60">
-            Moje konto
-          </ElevatedButton>
-        </NuxtLink>
-      </div>
+      <PanelTileLinks class="w-2/5" />
     </div>
 
-    <!-- Bottom 3 boxes -->
-    <div v-if="team_stats" class="col-span-1 flex divide-x font-pixelify text-center">
-      <div class="flex-1 p-10">
-        <div class="text-xl">
-          Miejsce drużyny
-        </div>
-        <div class="text-4xl font-bold">
-          {{ team_stats.rank?.[0] }}/{{ team_stats?.rank?.[1] }}
-        </div>
-      </div>
-      <div class="flex-1 p-10">
-        <div class="text-xl">
-          Zdobyte flagi
-        </div>
-        <div class="text-4xl text-green-400 font-bold">
-          {{ team_stats.captured_flags }}
-        </div>
-      </div>
-      <div class="flex-1 p-10">
-        <div class="text-xl">
-          Pozostałe flagi
-        </div>
-        <div class="text-4xl text-yellow-400 font-bold">
-          {{ team_stats.remaining_flags }}
-        </div>
-      </div>
-    </div>
+    <PanelTileStats v-if="team_stats" :team-stats="team_stats" class="col-span-1" />
   </div>
 </template>
