e5148012dfa34cf4b6d187714a03fdf541932ff3
norbiros <norbiros@protonmail.com>
2025-07-26 17:46:57 +0200

    fix(backend): ensure user existence in refresh token endpoint

--- backend/src/routes/auth/refresh.rs
+++ backend/src/routes/auth/refresh.rs
@@ -1,9 +1,11 @@
+use crate::entities::users;
 use crate::services::auth::AuthService;
+use crate::utils::app_state;
 use crate::utils::cookies::REFRESH_TOKEN_COOKIE;
 use crate::utils::error::Error;
-use actix_web::{HttpRequest, HttpResponse, post};
-
 use crate::utils::jwt::decode_jwt;
+use actix_web::{HttpRequest, HttpResponse, post, web};
+use sea_orm::EntityTrait;
 
 #[utoipa::path(
     responses(
@@ -16,15 +18,20 @@ use crate::utils::jwt::decode_jwt;
     tag = "auth"
 )]
 #[post("/refresh")]
-pub async fn refresh(request: HttpRequest) -> Result<HttpResponse, Error> {
+pub async fn refresh(
+    app_state: web::Data<app_state::AppState>,
+    request: HttpRequest,
+) -> Result<HttpResponse, Error> {
     let Some(refresh_token) = request.cookie(REFRESH_TOKEN_COOKIE) else {
         return Err(Error::Unauthorized);
     };
 
-    // todo: WE DO NOT CHECK IF USER EXISTS
-
     let claims = decode_jwt(refresh_token.value()).map_err(|_| Error::Unauthorized)?;
-    let response = AuthService::response_with_cookies(claims.claims.id, claims.claims.email)?;
+    users::Entity::find_by_id(claims.claims.id)
+        .one(&app_state.database)
+        .await?
+        .ok_or(Error::Unauthorized)?;
 
+    let response = AuthService::response_with_cookies(claims.claims.id, claims.claims.email)?;
     Ok(response)
 }
