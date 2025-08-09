e780c927ca8d3889a99b0433466fdd79aee08ece
wikipop <66514885+wikipop@users.noreply.github.com>
2025-08-09 16:57:48 +0200

    refactor(frontend): adjust Navbar layout and remove unused slots

--- frontend/app/components/LogoWithText.vue
+++ frontend/app/components/LogoWithText.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center text-default">
     <Logo class="size-8 mr-2" />
-    <NuxtImg src="/img/logo-text.webp" class="h-4" />
+    <NuxtImg src="/img/logo-text.webp" class="h-3.5" />
   </div>
 </template>
