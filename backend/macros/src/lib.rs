4a5157f673e377bde9fc304ec2c4f81e172aeca0
Kubaryt <kubastarzyk6554@gmail.com>
2025-08-14 11:35:13 +0200

    feat: Define names of objects in openapi based on file path not struct name in entities

--- backend/macros/src/lib.rs
+++ backend/macros/src/lib.rs
@@ -11,3 +11,8 @@ pub fn derive_updatable_model(input: TokenStream) -> TokenStream {
 pub fn error_with_messages(_: TokenStream, input: TokenStream) -> TokenStream {
     macros::localized_error::localized_error_impl(input)
 }
+
+#[proc_macro_attribute]
+pub fn to_schema_from_file_name(_attr: TokenStream, input: TokenStream) -> TokenStream {
+    macros::to_schema_from_file_name::to_schema_from_file_name(input)
+}
