ab0e89da9543789d0a1c01ff249fb0996781ae1a
wikipop <66514885+wikipop@users.noreply.github.com>
2025-08-15 01:36:11 +0200

    Revert "refactor(frontend): new LogoType"
    This reverts commit d7799187

--- frontend/app/components/LogoWithText.vue
+++ frontend/app/components/LogoWithText.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center text-default">
-    <Logo class="size-8 mr-2" />
-    <NuxtImg src="/img/logo-text.webp" class="h-3.5" />
+    <Logo class="size-[2em] mr-[1em]" />
+    <NuxtImg src="/img/logo-text.webp" class="h-[1em]" />
   </div>
 </template>
