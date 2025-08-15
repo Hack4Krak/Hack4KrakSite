50487790035dbbf2264fccb2ec807cfe44685c1e
wikipop <66514885+wikipop@users.noreply.github.com>
2025-08-15 03:32:59 +0200

    feat(frontend): add timeline component

--- frontend/app/pages/about_us.vue
+++ frontend/app/pages/about_us.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-
+import { aboutUsTimeline } from '~~/content/about-us-timeline'
 </script>
 
 <template>
-  <div class="flex flex-col gap-18 pt-12 sm:px-0 px-6">
+  <div class="flex flex-col gap-18 pt-12">
     <section
       id="hero"
       class="lg:h-[320px] md:h-[180px] relative flex flex-col justify-center w-full overflow-hidden
-                    sm:text-center sm:items-center text-left"
+             sm:text-center sm:items-center sm:px-0 px-6 text-left"
     >
-      <h1 class="heading-h3">
+      <h1 class="heading-h3 font-semibold text-lg">
         Hack4Krak
       </h1>
       <p class="max-w-[420px]">
@@ -19,10 +19,28 @@
       </p>
       <p
         class="lg:text-[320px] md:text-[220px] text-[180px] font-semibold absolute -z-10 text-primary
-                stroked-text-full-screen w-screen md:w-auto text-wrap"
+               stroked-text-full-screen w-screen md:w-auto text-wrap"
       >
         HACK4KRAK
       </p>
+    </section>
+    <section
+      id="timeline"
+      class="relative flex justify-center items-center"
+    >
+      <div class="flex md:flex-row flex-wrap flex-col gap-8 items-center justify-center">
+        <EventCard v-for="card in aboutUsTimeline" v-bind="card" :key="card.title" />
+      </div>
+      <NuxtImg
+        class="absolute h-[300px] left-0 md:top-auto top-[200px] -z-20" src="/img/blockAccentVector.svg"
+        alt="block accent vector"
+        height="300px"
+      />
+      <NuxtImg
+        class="absolute h-[300px] right-0 md:top-auto top-[600px] -z-20 rotate-180" src="/img/blockAccentVector.svg"
+        alt="block accent vector"
+        height="300px"
+      />
     </section>
   </div>
 </template>
@@ -32,7 +50,7 @@
   -webkit-text-stroke-color: transparent;
   -webkit-text-fill-color: var(--ui-bg);
   user-select: none;
-  background-color: --alpha(var(--accent-primary) / 20%);
+  background-color: --alpha(var(--color-accent-primary) / 20%);
   background-clip: text;
   -webkit-text-stroke-width: 0.02em;
 }
