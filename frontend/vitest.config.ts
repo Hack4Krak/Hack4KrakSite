9d0270cde1f3d45bde8fd80ba63640ed28e85358
norbiros <norbiros@protonmail.com>
2025-08-16 15:04:47 +0200

    fix(frontend): disable `ogImage` in vitest environment

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -6,5 +6,13 @@ export default defineVitestConfig({
     environment: 'nuxt',
     exclude: [...configDefaults.exclude, 'tests/e2e/**/*'],
     setupFiles: ['../tests/setup.ts'],
+    environmentOptions: {
+      nuxt: {
+        overrides: {
+          // @ts-expect-error Vitest doesn't recognize config options from modules
+          ogImage: { enabled: false },
+        },
+      },
+    },
   },
 })
