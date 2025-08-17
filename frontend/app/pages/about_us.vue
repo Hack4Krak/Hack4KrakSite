04fa6d0ab6a4d62c29d16aaa4cd2c89d68f6ad65
wikipop <66514885+wikipop@users.noreply.github.com>
2025-08-17 21:54:09 +0200

    feat(frontend): refactor about us layout with UContainer component

--- frontend/app/pages/about_us.vue
+++ frontend/app/pages/about_us.vue
@@ -3,20 +3,22 @@ import { aboutUsTimeline } from '~~/content/about-us-timeline'
 </script>
 
 <template>
-  <div class="flex flex-col gap-18 pt-12">
+  <div class="flex flex-col md:gap-20 gap-14 pt-12">
     <section
       id="hero"
-      class="lg:h-[320px] md:h-[180px] relative flex flex-col justify-center w-full overflow-hidden
-             sm:text-center sm:items-center sm:px-0 px-6 text-left"
+      class="lg:h-[320px] md:h-[180px] relative flex flex-col justify-center items-center w-full overflow-hidden
+             sm:text-center sm:items-center sm:px-0 text-left"
     >
-      <h1 class="heading-h3 font-semibold text-lg">
-        Hack4Krak
-      </h1>
-      <p class="max-w-[420px]">
-        to inicjatywa młodych liderów cyberbezpieczeństwa, których połączyła pasja do technologii, wyzwań i ciągłego
-        rozwoju. Tworzymy wydarzenia, które uczą, inspirują i integrują młodzież zainteresowaną światem IT i
-        bezpieczeństwa cyfrowego.
-      </p>
+      <UContainer class="flex flex-col justify-center items-center">
+        <h1 class="heading-h3 font-semibold text-lg">
+          Hack4Krak
+        </h1>
+        <p class="max-w-[420px]">
+          to inicjatywa młodych liderów cyberbezpieczeństwa, których połączyła pasja do technologii, wyzwań i ciągłego
+          rozwoju. Tworzymy wydarzenia, które uczą, inspirują i integrują młodzież zainteresowaną światem IT i
+          bezpieczeństwa cyfrowego.
+        </p>
+      </UContainer>
       <p
         class="lg:text-[320px] md:text-[220px] text-[180px] font-semibold absolute -z-10 text-primary
                stroked-text-full-screen w-screen md:w-auto text-wrap"
@@ -42,29 +44,32 @@ import { aboutUsTimeline } from '~~/content/about-us-timeline'
         height="300px"
       />
     </section>
-    <section id="mission" class="flex xl:flex-row flex-col-reverse mx-auto gap-12">
-      <div class="sm:w-160 flex-1 flex flex-col gap-4 px-6 sm:px-0">
-        <h3 class="heading-h3 text-lg font-semibold">
-          Nasza misja
-        </h3>
-        <p>
-          Naszą misją jest inspirowanie i wspieranie młodych ludzi w rozwijaniu pasji do cyberbezpieczeństwa. Tworzymy
-          przestrzeń opartą na:
-        </p>
-        <div class="flex flex-wrap gap-4">
-          <Label>zaangażowaniu</Label>
-          <Label>praktycznej nauce</Label>
-          <Label>dzieleniu się wiedzą</Label>
-          <Label>rozwoju kompetencji przyszłości</Label>
+    <section id="mission">
+      <UContainer class="flex lg:flex-row flex-col-reverse mx-auto gap-12">
+        <div class="flex-1 flex flex-col gap-4 px-6 sm:px-0">
+          <h3 class="heading-h3 text-lg font-semibold">
+            Nasza misja
+          </h3>
+          <p>
+            Naszą misją jest inspirowanie i wspieranie młodych ludzi w rozwijaniu pasji do cyberbezpieczeństwa. Tworzymy
+            przestrzeń opartą na:
+          </p>
+          <div class="flex flex-wrap gap-4">
+            <Label>zaangażowaniu</Label>
+            <Label>praktycznej nauce</Label>
+            <Label>dzieleniu się wiedzą</Label>
+            <Label>rozwoju kompetencji przyszłości</Label>
+          </div>
+          <p>
+            Wierzymy, że edukacja w zakresie bezpieczeństwa cyfrowego powinna być dostępna, motywująca i prowadzona
+            przez
+            tych, którzy najlepiej rozumieją swoje pokolenie.
+          </p>
         </div>
-        <p>
-          Wierzymy, że edukacja w zakresie bezpieczeństwa cyfrowego powinna być dostępna, motywująca i prowadzona przez
-          tych, którzy najlepiej rozumieją swoje pokolenie.
-        </p>
-      </div>
-      <div class="flex-1 justify-center items-center hidden sm:flex">
-        <NuxtImg class="w-full px-6 sm:px-0" src="/img/flag_n_clouds.webp" />
-      </div>
+        <div class="flex-1 justify-center items-center hidden sm:flex">
+          <NuxtImg class="w-full px-6 sm:px-0" src="/img/flag_n_clouds.webp" />
+        </div>
+      </UContainer>
     </section>
     <Footer />
   </div>
