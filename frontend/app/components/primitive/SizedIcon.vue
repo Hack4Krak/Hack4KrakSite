37f86c0561ae9f6b618148918a64044f423f25e6
wikipop <66514885+wikipop@users.noreply.github.com>
2025-08-19 16:14:38 +0200

    feat(frontend): introduce SizedIcon component

---
+++ frontend/app/components/primitive/SizedIcon.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+type IconSize = 'small' | 'medium' | 'large'
+
+const props = withDefaults(defineProps<{
+  format?: IconSize
+  name: string
+}>(), {
+  format: 'small',
+})
+
+// const size = computed(() => props.format === 'large' ? '7' : '4')
+const size = computed(() => {
+  switch (props.format) {
+    case 'large':
+      return '7'
+    case 'medium':
+      return '5'
+    default:
+      return '4'
+  }
+})
+</script>
+
+<template>
+  <Icon
+    :name="name" :style="{
+      width: `calc(var(--spacing) * ${size})`,
+      height: `calc(var(--spacing) * ${size})`,
+    }"
+  />
+</template>
