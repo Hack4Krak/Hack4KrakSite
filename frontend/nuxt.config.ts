9ffa4b08e8052716f296ade99208142d3604c99e
wikipop <66514885+wikipop@users.noreply.github.com>
2025-08-20 18:25:58 +0200

    feat(frontend): add prerendering for about us page

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -68,6 +68,7 @@ export default defineNuxtConfig({
     '/tasks/description/**': { swr: true },
     '/docs/**': { prerender: true },
     '/': { prerender: true },
+    '/about_us': { prerender: true },
     '/faq': { redirect: '/docs/faq' },
     '/rules': { redirect: '/docs/rules' },
   },
