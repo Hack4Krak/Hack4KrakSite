2ee796e7e319fec097432baf460367c4dcafeafc
wikipop <66514885+wikipop@users.noreply.github.com>
2025-08-30 20:30:40 +0200

    refactor(frontend): update MobileLogoSection to use `logo-no-text.svg`

--- frontend/app/components/hero/MobileLogoSection.vue
+++ frontend/app/components/hero/MobileLogoSection.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="lg:hidden flex place-content-center">
-    <NuxtImg src="/img/sygnet.svg" class="max-h-24" />
+    <NuxtImg src="/img/logo-no-text.svg" class="max-h-24" />
   </div>
 </template>
