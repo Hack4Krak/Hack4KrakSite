af5720b4d6d7cdec7b64ff1492f2b68db04e57f6
wikipop <66514885+wikipop@users.noreply.github.com>
2025-09-09 20:33:14 +0200

    feat(frontend): about us page (#551)
    Create a new `/about_us` page detailing our history, past events, and
    future plans. Includes rich copywriting content and illustrative images.
    
    Closes #551

---
+++ frontend/content/about-us-content.ts
@@ -0,0 +1,34 @@
+export const aboutUsContent = {
+  meta: {
+    title: 'O nas',
+  },
+  hero: {
+    title: 'Hack4Krak',
+    description: 'to inicjatywa młodych liderów cyberbezpieczeństwa, których połączyła pasja do technologii, wyzwań i ciągłego rozwoju. Tworzymy wydarzenia, które uczą, inspirują i integrują młodzież zainteresowaną światem IT i bezpieczeństwa cyfrowego.',
+    backgroundText: 'HACK4KRAK',
+  },
+  mission: {
+    title: 'Nasza misja',
+    description: 'Naszą misją jest inspirowanie i wspieranie młodych ludzi w rozwijaniu pasji do cyberbezpieczeństwa. Tworzymy przestrzeń opartą na:',
+    labels: [
+      'zaangażowaniu',
+      'praktycznej nauce',
+      'dzieleniu się wiedzą',
+      'rozwoju kompetencji przyszłości',
+    ],
+    additionalText: 'Wierzymy, że edukacja w zakresie bezpieczeństwa cyfrowego powinna być dostępna, motywująca i prowadzona przez tych, którzy najlepiej rozumieją swoje pokolenie.',
+  },
+  gallery: {
+    title: 'Inicjatywa stworzona przez uczniów – dla uczniów.',
+    description: [
+      'Jesteśmy grupą młodych entuzjastów cyberbezpieczeństwa z Krakowa, których połączyła pasja do technologii, wyzwań i ciągłego rozwoju.',
+      'Działamy jako niezależna organizacja, której celem jest promowanie wiedzy z zakresu cyberbezpieczeństwa wśród młodzieży poprzez organizację wydarzeń typu CTF (Capture The Flag) – konkursów, w których uczestnicy rozwiązują zadania z obszaru bezpieczeństwa cyfrowego.',
+    ],
+    smallGalleryImages: [
+      '/img/about-us/gallery1.webp',
+      '/img/about-us/gallery2.webp',
+      '/img/about-us/gallery3.webp',
+    ],
+    mainGalleryImage: '/img/our-team.webp',
+  },
+}
---
+++ frontend/content/about-us-timeline.ts
@@ -0,0 +1,42 @@
+export interface EventCardProps {
+  title: string
+  subtitle: string
+  img: string
+  imgCrop?: string
+  participants: number
+  description: string
+  addColorAccent?: boolean
+}
+
+export const aboutUsTimeline: EventCardProps[] = [
+  {
+    title: 'Hack4Krak CTF dla uczniów 31 LO w Krakowie',
+    subtitle: 'Luty 2025',
+    description: ''
+      + '**67 uczniów** rozwiązywało zadania z zakresu cyberbezpieczeństwa, rozwijając przy tym swoje umiejętności '
+      + 'techniczne, logiczne myślenie oraz pracę zespołową.\n\n'
+      + 'Wydarzenie pozwoliło również na przetestowanie i potwierdzenie działania naszej autorskiej platformy.',
+    participants: 67,
+    img: '/img/events/event1.webp',
+  },
+  {
+    title: 'Hack4Krak CTF dla uczniów szkół podstawowych',
+    subtitle: 'Maj 2025',
+    description: '**123 uczniów** szkół podstawowych z Krakowa wzięło udział w drugiej edycji Hack4Krak CTF. \n\n'
+      + 'Dla wielu było to pierwsze zetknięcie z tematyką cyberbezpieczeństwa. Zadania, dostosowane do ich poziomu, '
+      + 'rozwijały kluczowe kompetencje przyszłości i uczyły przez zabawę, inspirując do dalszej nauki.',
+    participants: 123,
+    img: '/img/events/event2.webp',
+  },
+  {
+    title: 'Przygotowania do największej edycji Hack4Krak CTF',
+    subtitle: 'Marzec 2026',
+    participants: 250,
+    description: 'Trzecia edycja **Hack4Krak CTF** będzie stacjonarnym wydarzeniem dla uczniów szkół średnich. \n\n'
+      + 'Planujemy przyciągnąć setki uczestników, oferując angażujące zadania i rozwijając kluczowe umiejętności. '
+      + 'Wydarzenie ma podnieść standard konkursów cyberbezpieczeństwa dla młodzieży w całej Polsce.',
+    img: '/img/our-team.webp',
+    imgCrop: '900_100_3500_1700',
+    addColorAccent: true,
+  },
+]
