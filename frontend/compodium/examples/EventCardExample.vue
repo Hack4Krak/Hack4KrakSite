231394190fd072fcf1047586c174cbe4e8ee4276
wikipop <66514885+wikipop@users.noreply.github.com>
2025-09-09 20:33:14 +0200

    feat(frontend): about us page (#551)
    Create a new `/about_us` page detailing our history, past events, and
    future plans. Includes rich copywriting content and illustrative images.
    
    Closes #551

---
+++ frontend/compodium/examples/EventCardExample.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import EventCard from '../../app/components/EventCard.vue'
+</script>
+
+<template>
+  <EventCard img="/img/events/event1.webp" participants="12" title="Hack4Krak CTF dla uczniów 31 LO w Krakowie" subtitle="Subtitle" />
+</template>
