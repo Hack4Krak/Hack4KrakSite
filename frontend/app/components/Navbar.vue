06bfb2405d3e44ee2ec21e6cbf806bbf4458608d
norbiros <norbiros@protonmail.com>
2025-09-24 15:17:10 +0200

    refactor(frontend): migrate `Navbar` component to `UHeader`
    It should look exactly the same, bug code is 30 lines smaller, and
    mobile navigation UX is slightly improved.

--- frontend/app/components/Navbar.vue
+++ frontend/app/components/Navbar.vue
@@ -1,89 +1,58 @@
 <script setup lang="ts">
 import { NAVBAR_ITEMS } from '~~/content/navbar'
 
-const [DefineNavbarTemplate, ReuseNavbarTemplate] = createReusableTemplate()
-
-const isMobileMenuOpen = ref(false)
-
 const { data: isLoggedIn } = useAuth('/auth/status', {
   redirect: 'error',
   onResponseError: undefined,
 })
 
-function toggleMobileMenu() {
-  isMobileMenuOpen.value = !isMobileMenuOpen.value
-}
-
-// Hide navbar on button press
-const router = useRouter()
-watch(() => router.currentRoute.value, () => {
-  isMobileMenuOpen.value = false
-})
+const navigationMenuProperties = computed(() => ({
+  'content-orientation': 'vertical' as const,
+  'items': NAVBAR_ITEMS,
+  'variant': 'link' as const,
+  'ui': {
+    linkLabel: 'hover:underline underline-offset-5 text-md',
+    viewport: 'w-(--reka-navigation-menu-viewport-width)',
+    childList: 'flex-col items-center',
+    link: 'text-md text-default data-active:text-secondary',
+    list: 'gap-8',
+  },
+}))
 </script>
 
 <template>
-  <DefineNavbarTemplate v-slot="{ orientation }">
-    <div class="flex w-full">
-      <div class="md:flex hidden flex-1 items-center">
-        <NuxtLink to="/">
-          <LogoWithText />
-        </NuxtLink>
-      </div>
-      <div>
-        <UNavigationMenu
-          content-orientation="vertical"
-          :orientation="orientation"
-          :items="NAVBAR_ITEMS" variant="link" class="w-full" color="error"
-          :ui="{
-            linkLabel: 'hover:underline underline-offset-5 text-md',
-            viewport: 'w-(--reka-navigation-menu-viewport-width)',
-            childList: 'flex-col items-center',
-            link: 'text-md text-default data-active:text-secondary',
-            list: 'gap-8',
-          }"
-        />
-      </div>
-      <div class="md:flex hidden flex-1 justify-end items-center">
-        <NuxtLink to="/login" class="text-md font-semibold">
+  <UHeader :ui="{ container: 'max-w-full' }" class="bg-default">
+    <template #title>
+      <LogoWithText class="text-base" />
+    </template>
+
+    <UNavigationMenu v-bind="navigationMenuProperties" />
+
+    <template #right>
+      <NuxtLink to="/login" class="text-md font-semibold flex justify-end w-full" :aria-label="isLoggedIn ? 'Otwórz panel' : 'Zaloguj się'">
+        <UIcon :name="isLoggedIn ? 'pixelarticons:user' : 'pixelarticons:login'" class="icon-md lg:hidden" />
+
+        <span class="hidden lg:inline">
           {{ isLoggedIn ? "Otwórz panel" : "Zaloguj się" }}
-        </NuxtLink>
-      </div>
-    </div>
-  </DefineNavbarTemplate>
-
-  <UContainer class="sticky top-0 max-w-full font-sans bg-default z-20 print:hidden">
-    <div class="hidden md:flex justify-center items-center h-(--ui-header-height)">
-      <ReuseNavbarTemplate orientation="horizontal" />
-    </div>
-
-    <!-- Mobile Navigation -->
-    <div class="md:hidden flex gap-4 py-2">
-      <NuxtLink to="/" class="flex items-center">
-        <LogoWithText />
+        </span>
       </NuxtLink>
-      <NuxtLink to="/login" class="flex items-center text-md font-semibold ml-auto" :aria-label="isLoggedIn ? 'Otwórz panel' : 'Zaloguj się'">
-        <UIcon :name="isLoggedIn ? 'pixelarticons:user' : 'pixelarticons:login'" class="icon-md" />
-      </NuxtLink>
+
+      <div class="md:flex hidden flex-1 justify-end items-center" />
+    </template>
+
+    <template #toggle="{ open, toggle }">
       <button
-        class="p-2 cursor-pointer flex justify-center"
+        class="p-2 ml-2 cursor-pointer flex justify-center lg:hidden"
         aria-label="Przełącz nawigacje mobilną"
         data-testid="mobile-menu-toggle"
-        @click="toggleMobileMenu"
+        @click="toggle"
       >
-        <UIcon :name="isMobileMenuOpen ? 'pixelarticons:close' : 'pixelarticons:menu'" class="icon-lg" />
+        <UIcon :name="open ? 'pixelarticons:close' : 'pixelarticons:menu'" class="icon-lg" />
       </button>
-    </div>
+    </template>
 
-    <Transition
-      enter-from-class="opacity-0 translate-x-[100%]"
-      leave-to-class="opacity-0 translate-x-[100%]"
-      enter-active-class="transition duration-200"
-      leave-active-class="transition duration-200"
-      hydrate-on-media-query="(max-width: 768px)"
-    >
-      <div v-if="isMobileMenuOpen" class="my-2 md:hidden h-screen [&>a]:text-5xl ">
-        <ReuseNavbarTemplate orientation="vertical" />
-      </div>
-    </Transition>
-  </UContainer>
+    <template #body>
+      <UNavigationMenu v-bind="navigationMenuProperties" orientation="vertical" class="-mx-2.5" />
+    </template>
+  </UHeader>
 </template>
