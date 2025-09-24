6616a3cd41a3f44ca624dfae03c29120522d1bd4
norbiros <norbiros@protonmail.com>
2025-09-24 11:57:19 +0200

    perf(frontend): remove `theme(static)` from `@import 'tailwindcss'`
    This change broke `InfoTitle` component, which was dynamically
    accessing colors, which were always registered before. Now everything
    should look the same but use less traffic.

--- frontend/content/info-tiles.ts
+++ frontend/content/info-tiles.ts
@@ -2,25 +2,25 @@ export const INFO_TILES_CONTENT = [
   {
     front: 'Na czym polegają <br> **zadania** podczas <br> wydarzenia?',
     back: 'Celem każdej drużyny jest **rozwiązywanie zadań** z różnych dziedzin **cyberbezpieczeństwa**. Za każde poprawnie rozwiązane zadanie drużyna otrzymuje punkty, które zadecydują o końcowym wyniku.',
-    color: 'green-400',
+    color: '#05df72', // green-400
     image: '/img/character/knight.png',
   },
   {
     front: '**Kto** może <br>wziąć udział?',
     back: 'W wydarzeniu mogą wziąć udział **uczniowie szkół ponadpodstawowych**, którzy chcą spróbować swoich sił w świecie cyberbezpieczeństwa.',
-    color: 'yellow-300',
+    color: '#dab2ff', // purple-300
     image: '/img/character/witch_tower.png',
   },
   {
     front: 'Kiedy odbędzie się **kolejne** wydarzenie?',
     back: 'Na ten moment nie ma dokładnej daty. Planowany termin to **2025/26 rok**. Śledź nasze **social media**, by nie przegapić wiadomości o kolejnej edycji.',
-    color: 'purple-300',
+    color: '#ffdf26', // yellow-300
     image: '/img/character/king.png',
   },
   {
     front: 'Czym jest <br>**Hack4Krak**?',
     back: '**Hack4Krak** to grupa krakowskich pasjonatów informatyki, która zajmuje się tworzeniem i organizacją wydarzeń typu **CTF Jeopardy** z zakresu cyberbezpieczeństwa, skierowanych do uczniów **szkół w całej Polsce**.',
-    color: 'yellow-100',
+    color: '#fef9c2', // yellow-100
     image: '/img/character/princess.png',
   },
 ]
