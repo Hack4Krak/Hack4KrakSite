8b6b085f0ff508dd11d43b21ccd89aa6d54ea58d
norbiros <norbiros@protonmail.com>
2025-09-24 13:23:36 +0200

    refactor(frontend): switch to `UMain` component
    In our codebase we were using a lot of `<main>` tags. Now it should be
    all unified. `<UApp>` is only in one place - `app.vue`. Thanks to
    `min-h` inside `UMain` component I was able to standardize page
    `min-height` (#688)

--- frontend/app/layouts/centered.vue
+++ frontend/app/layouts/centered.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLayout name="default">
     <div
-      class="flex flex-col justify-center items-center w-full min-h-screen-without-header
+      class="flex flex-col justify-center items-center w-full min-h-[inherit]
         bg-[url(assets/img/background.webp)] bg-cover rendering-pixelated"
     >
       <div class="my-5 p-8 rounded-2xl space-y-4 sm:min-w-90 min-w-70 bg-default">
--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -1,11 +1,6 @@
 <template>
-  <main>
-    <NuxtLoadingIndicator color="var(--ui-primary)" :height="2" />
-    <UApp>
-      <Navbar />
-      <div class="flex min-h-screen-without-header">
-        <slot />
-      </div>
-    </UApp>
-  </main>
+  <Navbar />
+  <UMain>
+    <slot />
+  </UMain>
 </template>
