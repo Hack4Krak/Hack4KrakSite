f05cf346cd27d0186a0f85ee74209e1f1733a8c6
lajczi <lajczi@disroot.org>
2026-01-30 16:22:06 +0100

    fix(frontend): load `.env` from project root
    Signed-off-by: lajczi <lajczi@disroot.org>

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '../.env' })
+
 const backendAddress = process.env.BACKEND_ADDRESS || 'http://localhost:8080'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
