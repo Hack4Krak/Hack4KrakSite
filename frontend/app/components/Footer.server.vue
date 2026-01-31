7356e477f296972a002ce06e3e09c4cd89c20d8f
lajczi <lajczi@disroot.org>
2026-01-31 22:57:54 +0100

    feat: update footer to match design
    Signed-off-by: lajczi <lajczi@disroot.org>

--- frontend/app/components/Footer.server.vue
+++ frontend/app/components/Footer.server.vue
@@ -6,12 +6,12 @@ const runtimeConfig = useRuntimeConfig().public
 
 <template>
   <USeparator />
-  <UFooter class="w-full text-muted py-8 lg:px-12 lg:mb-8 text-xs lg:text-sm">
+  <UFooter class="w-full text-muted py-8 lg:px-12 lg:mb-8 text-xs lg:text-base">
     <template #left>
-      <div class="flex flex-col items-center lg:items-baseline justify-between gap-2 mb-5 lg:mb-0- text-xs lg:text-sm">
-        <img src="~/assets/img/logo.svg" class="h-(--spacing-icon-lg)" alt="Hack4Krak logo">
+      <div class="flex flex-col items-center lg:items-start justify-between text-xs lg:text-base">
+        <img src="~/assets/img/logo.svg" class="h-(--spacing-icon-lg) mb-5 lg:mb-0" alt="Hack4Krak logo">
 
-        <p class="text-center lg:text-left">
+        <p class="text-center lg:text-left mt-0 lg:mt-6">
           © {{ new Date().getFullYear() }} Wszelkie prawa zastrzeżone.
         </p>
 
@@ -22,13 +22,13 @@ const runtimeConfig = useRuntimeConfig().public
           </NuxtLink>!
         </div>
         <div v-if="runtimeConfig.gitCommit" class="hidden lg:block">
-          <span aria-hidden="true" class="text-xs text-dimmed">{{ runtimeConfig.gitCommit }}@{{ runtimeConfig.gitBranch }}</span>
+          <span aria-hidden="true" class="text-xs text-dimmed">{{ runtimeConfig.gitBranch }}@{{ runtimeConfig.gitCommit }}</span>
         </div>
       </div>
     </template>
 
     <template #right>
-      <div class="text-center w-full lg:grid grid-rows-2 grid-flow-col-dense place-content-between">
+      <div class="text-center w-full lg:grid grid-rows-2 grid-flow-col-dense place-content-center lg:place-content-between">
         <ULink
           v-for="{ label, icon, to, target } in FOOTER_SOCIALS"
           :key="label"
