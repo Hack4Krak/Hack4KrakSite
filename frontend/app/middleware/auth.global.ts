8706a713afd61e209049246322cc649ff3c156fd
lajczi <lajczi@disroot.org>
2026-03-03 17:16:29 +0100

    feat: add callback url to `submit_personal_info` page
    Signed-off-by: lajczi <lajczi@disroot.org>

--- frontend/app/middleware/auth.global.ts
+++ frontend/app/middleware/auth.global.ts
@@ -9,7 +9,7 @@ export default defineNuxtRouteMiddleware(async (to) => {
       }
       if (to.path.startsWith('/panel')) {
         if (data.value.has_personal_information === false) {
-          return '/account/submit_personal_info'
+          return await navigateTo({ path: '/account/submit_personal_info', query: { callback: to.fullPath } })
         }
       }
     } catch {
