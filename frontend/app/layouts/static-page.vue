99de1204140cd5acf61ba9967f2eac3e03270665
lajczi <lajczi@disroot.org>
2026-03-10 20:37:16 +0100

    fix(frontend): fix footer width on static pages
    Signed-off-by: lajczi <lajczi@disroot.org>

--- frontend/app/layouts/static-page.vue
+++ frontend/app/layouts/static-page.vue
@@ -3,6 +3,8 @@
     <HeroDesktopHeroBackground class="h-screen fixed top-(--ui-header-height)" />
     <UContainer class="flex flex-col justify-center items-center mt-10 lg:mt-30 mb-5 bg-default rounded-2xl">
       <slot />
+    </UContainer>
+    <UContainer>
       <Footer class="mt-8 lg:mt-12" />
     </UContainer>
   </NuxtLayout>
