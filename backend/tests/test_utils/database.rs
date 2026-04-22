e61420d122710356d58dc599ed733afa18ec393e
Sanielia <me@sanielia.dev>
2026-04-22 20:10:27 +0200

    refactor(backend): Move email_verification_request creation to test_utils::database

--- backend/tests/test_utils/database.rs
+++ backend/tests/test_utils/database.rs
@@ -1,8 +1,9 @@
 use crate::test_utils;
 use chrono::Utc;
 use hack4krak_backend::entities::sea_orm_active_enums::{TeamStatus, UserRoles};
-use hack4krak_backend::entities::{teams, users};
+use hack4krak_backend::entities::{email_verification_request, teams, users};
 use sea_orm::{DatabaseConnection, EntityTrait};
+use serde_json::json;
 use uuid::Uuid;
 
 #[derive(Default)]
@@ -73,6 +74,39 @@ impl TestDatabase {
             .unwrap();
 
         teams::Entity::find_by_id(team_uuid)
+            .one(&self.database)
+            .await
+            .unwrap()
+            .unwrap()
+    }
+
+    pub async fn with_email_verification_request(
+        &self,
+        updatable_model: email_verification_request::UpdatableModel,
+    ) -> email_verification_request::Model {
+        let confirmation_code = Uuid::new_v4();
+        let updated = updatable_model.update(email_verification_request::Model {
+            id: confirmation_code,
+            email: "".to_string(),
+            action_type: "confirm_email_address".to_string(),
+            additional_data: Some(json![{
+            "user_information": {
+                "name": "test_user",
+                "first_name": "dziengiel",
+                "email": "example@gmail.com",
+                "password_hash": "$argon2id$v=19$m=19456,t=2,p=1$nTzWdmrtGEOnwCocrg76xg$yv16FfDT5+meKwPmSiV+MF9kP8Man6bXZs+BloFTKIk".to_string(),
+            }
+            }]),
+            expiration_time: Some(Utc::now().naive_utc() + chrono::Duration::minutes(30)),
+            created_at: Utc::now().naive_utc(),
+        });
+
+        email_verification_request::Entity::insert(updated)
+            .exec(&self.database)
+            .await
+            .unwrap();
+
+        email_verification_request::Entity::find_by_id(confirmation_code)
             .one(&self.database)
             .await
             .unwrap()
