6e5009cc075d2a844bdd43db1a632ad0dff59e18
norbiros <norbiros@protonmail.com>
2026-04-29 18:38:40 +0200

    feat(frontend): update `centered` layout styling

--- frontend/app/layouts/centered.vue
+++ frontend/app/layouts/centered.vue
@@ -1,9 +1,14 @@
 <template>
   <NuxtLayout name="default">
     <div
-      class="flex w-full flex-1 bg-default sm:flex-col sm:items-center sm:justify-center sm:bg-[url(assets/img/background.webp)] bg-cover endering-pixelated"
+      class="flex w-full flex-1 bg-default bg-cover rendering-pixelated
+             sm:flex-col sm:items-center sm:justify-centersm:bg-[url(assets/img/background.webp)]"
     >
-      <div class="min-h-full w-full bg-default px-6 py-6 sm:my-5 sm:min-h-0 sm:w-auto sm:min-w-90 sm:space-y-4 sm:rounded-2xl sm:p-8">
+      <div
+        class="min-h-full w-full bg-default px-6 py-6 sm:my-5 sm:min-h-0 sm:w-auto sm:min-w-90 sm:space-y-4
+               sm:rounded-2xl sm:border-2 sm:border-surface-muted sm:p-8
+               sm:shadow-[4px_4px_0_var(--color-primary)]"
+      >
         <slot />
       </div>
     </div>
