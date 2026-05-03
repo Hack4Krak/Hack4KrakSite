566c5e4d33fee4757cc4e5b932b92d9a65f6bea7
P0midor <oberwankacper@gmail.com>
2026-05-03 18:06:59 +0200

    feat(frontend): add highlight in 'zarząd' section

--- frontend/app/components/TeamMemberCard.vue
+++ frontend/app/components/TeamMemberCard.vue
@@ -3,13 +3,17 @@ const props = defineProps<{
   name: string
   email: string
   role: string
+  highlight?: boolean
   github?: string
   linkedin?: string
 }>()
 </script>
 
 <template>
-  <div class="flex flex-col gap-1 transition duration-200 border-2 border-muted hover:border-primary text-center min-w-60 w-80">
+  <div
+    class="flex flex-col gap-1 transition duration-200 border-2 text-center min-w-60 w-80"
+    :class="{ 'border-muted hover:border-primary': !props.highlight, 'border-primary bg-primary/5 hover:bg-primary/10': props.highlight }"
+  >
     <div class="flex justify-center items-center">
       {{ props.name }}
       <a
@@ -19,7 +23,12 @@ const props = defineProps<{
         rel="noopener noreferrer"
         class="inline-flex items-center"
       >
-        <UIcon v-if="props.linkedin" name="mdi:linkedin" size="16px" class="ml-1.5 transition-transform duration-400 hover:scale-125" />
+        <UIcon
+          v-if="props.linkedin"
+          name="mdi:linkedin"
+          size="16px"
+          class="ml-1.5 transition-transform duration-400 hover:scale-125"
+        />
       </a>
       <a
         v-if="props.github"
@@ -28,7 +37,12 @@ const props = defineProps<{
         rel="noopener noreferrer"
         class="inline-flex items-center"
       >
-        <UIcon v-if="props.github" name="mdi:github" size="16px" class="ml-1.5 transition-transform duration-400 hover:scale-125" />
+        <UIcon
+          v-if="props.github"
+          name="mdi:github"
+          size="16px"
+          class="ml-1.5 transition-transform duration-400 hover:scale-125"
+        />
       </a>
     </div>
     <div class="text-muted">
--- frontend/app/components/section/Organizers.vue
+++ frontend/app/components/section/Organizers.vue
@@ -1,20 +1,44 @@
 <script setup lang="ts">
 import { organizers } from '~~/content/organizers'
+
+const groupedOrganizers = computed(() => {
+  const map = new Map<string, typeof organizers.team>()
+
+  for (const member of organizers.team) {
+    if (!map.has(member.section)) {
+      map.set(member.section, [])
+    }
+
+    map.get(member.section)!.push(member)
+  }
+
+  return map
+})
 </script>
 
 <template>
   <div class="flex flex-col gap-6">
-    <section v-for="sectionName in organizers.hierarchy" :key="sectionName" class="flex flex-col w-full text-center items-center justify-center">
-      <h3 class="font-semibold font-neutral-50">
+    <section
+      v-for="sectionName in organizers.hierarchy"
+      :key="sectionName"
+      class="flex flex-col w-full text-center items-center justify-center"
+    >
+      <h3 class="font-semibold">
         {{ sectionName.toUpperCase() }}
       </h3>
       <div class="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-6">
-        <TeamMemberCard v-for="organizer in organizers.team.filter(member => member.section === sectionName)" :key="organizer.name" :name="organizer.name" :email="organizer.email" :role="organizer.role" :github="organizer.github" :linkedin="organizer.linkedin" class="w-full" />
+        <TeamMemberCard
+          v-for="organizer in groupedOrganizers.get(sectionName) || []"
+          :key="organizer.email"
+          :name="organizer.name"
+          :email="organizer.email"
+          :role="organizer.role"
+          :github="organizer.github"
+          :linkedin="organizer.linkedin"
+          :highlight="sectionName === 'zarząd'"
+          class="w-full"
+        />
       </div>
     </section>
   </div>
 </template>
-
-<style scoped>
-
-</style>
