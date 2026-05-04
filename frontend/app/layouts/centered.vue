aac85d06e4cba8b33fedcd33c9ee0cff21ef2387
norbiros <norbiros@protonmail.com>
2026-05-04 12:57:42 +0200

    feat(frontend): do not display background for centered layout on mobile

--- frontend/app/layouts/centered.vue
+++ frontend/app/layouts/centered.vue
@@ -1,10 +1,9 @@
 <template>
   <NuxtLayout name="default">
     <div
-      class="flex flex-col justify-center items-center w-full flex-1
-        bg-[url(assets/img/background.webp)] bg-cover rendering-pixelated"
+      class="flex w-full flex-1 bg-default sm:flex-col sm:items-center sm:justify-center sm:bg-[url(assets/img/background.webp)] bg-cover endering-pixelated"
     >
-      <div class="my-5 p-8 rounded-2xl space-y-4 sm:min-w-90 min-w-70 bg-default">
+      <div class="min-h-full w-full bg-default px-6 py-6 sm:my-5 sm:min-h-0 sm:w-auto sm:min-w-90 sm:space-y-4 sm:rounded-2xl sm:p-8">
         <slot />
       </div>
     </div>
