59600dd3207d25941cd4e767891f0b13aad0d6f3
norbiros <norbiros@protonmail.com>
2026-05-09 11:40:55 +0200

    fix(frontend): correct invalid tailwind classes in the centered layout

--- frontend/app/layouts/centered.vue
+++ frontend/app/layouts/centered.vue
@@ -2,7 +2,7 @@
   <NuxtLayout name="default">
     <div
       class="flex w-full flex-1 bg-default bg-cover rendering-pixelated
-             sm:flex-col sm:items-center sm:justify-centersm:bg-[url(assets/img/background.webp)]"
+             sm:flex-col sm:items-center sm:justify-center sm:bg-[url(assets/img/background.webp)]"
     >
       <div
         class="min-h-full w-full bg-default px-6 py-6 sm:my-5 sm:min-h-0 sm:w-auto sm:min-w-90 sm:space-y-4
